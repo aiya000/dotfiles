b4123daa5089d7700501c945c59d3b5eb4e3e579
aiya000 <aiya000.develop@gmail.com>
2016-04-13 10:08:44 +0900

    Clean bin/**
    Create bin/dot-link.sh
    Remove .gitmodules

---
+++ bin/dot-link.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -eu
+
+echo '*** Warning ***'
+echo 'This script force overwrite dotfiles that under $HOME'
+echo '***************'
+echo -n 'Do link dotfiles (y/n): '
+read confirm
+if [ "$confirm" != "y" ] ; then
+	exit 0
+fi
+
+dotfiles_dir="${HOME}/.dotfiles"
+cat "${dotfiles_dir}/bin/target.txt" | xargs -I {} ln -s "${dotfiles_dir}/{}" "${HOME}/{}" \
+	&& echo 'suceed.' \
+	|| echo 'failed.'
