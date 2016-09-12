405ef6d8007fba8c2a15f466061633b58c2f3f86
aiya000 <aiya000.develop@gmail.com>
2016-09-12 10:08:45 +0900

    Organize Files/** directories
    Update .xmobarrc
    Update xmonad.hs
    Update .private submodule
    Delete Note/bashrc.txt

--- Files/root/home/foo/.xmonad/xmonad.hs
+++ Files/root/home/foo/.xmonad/xmonad.hs
@@ -8,25 +8,28 @@
 --     - xmonad-extras-darcs
 
 import Control.Monad ((>=>))
+import Text.Printf (printf)
 import XMonad
 import XMonad.Actions.CycleWS (nextScreen)
 import XMonad.Actions.Volume (toggleMute, lowerVolume, raiseVolume)
 import XMonad.Config.Desktop (desktopConfig)
 import XMonad.Hooks.DynamicLog (xmobar)
 import XMonad.Hooks.ManageDocks (avoidStruts)
+import XMonad.Hooks.UrgencyHook (UrgencyHook(..), withUrgencyHook)
 import XMonad.Layout.Gaps (GapMessage(..))
 import XMonad.Layout.MultiToggle (Toggle(..))
 import XMonad.Layout.MultiToggle.Instances (StdTransformers(..))
 import XMonad.Layout.ResizableTile (MirrorResize(..))
 import XMonad.Layout.Tabbed (simpleTabbed)
-import XMonad.StackSet (focusUp, focusDown, swapUp, swapDown)
+import XMonad.StackSet (focusUp, focusDown, swapUp, swapDown, findTag)
 import XMonad.Util.Dzen (DzenConfig, dzenConfig, onCurr, center, addArgs)
 import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.NamedWindows (getName)
 import XMonad.Util.SpawnOnce (spawnOnce)
 
 
 main :: IO ()
-main = (xmobar >=> xmonad) $ desktopConfig
+main = (xmobar >=> xmonad) $ withUrgencyHook LibNotifyHook $ desktopConfig
   { terminal    = "xterm"
   , modMask     = superMask
   , borderWidth = 2
@@ -37,7 +40,20 @@ main = (xmobar >=> xmonad) $ desktopConfig
   `additionalKeys` myKeymappings
 
 
--- Variables
+-- Data types
+
+-- See https://pbrisbin.com/posts/using_notify_osd_for_xmonad_notifications/
+data LibNotifyHook = LibNotifyHook deriving (Read, Show)
+instance UrgencyHook LibNotifyHook where
+  urgencyHook LibNotifyHook w = do
+    name       <- getName w
+    maybeIndex <- findTag w <$> gets windowset
+    case maybeIndex of
+      Nothing    -> notifySend [">> Fatal error on the urgencyHook"]
+      Just index -> notifySend [show name, "workspace '" ++ index ++ "'"]
+
+
+-- The functions and the values
 firstTerminal :: String
 firstTerminal = "xfce4-terminal"
 
@@ -47,6 +63,14 @@ altMask = mod1Mask
 superMask :: KeyMask
 superMask = mod4Mask
 
+espeak :: String -> X ()
+espeak msg = spawn $ "espeak -s 150 -v +fex \"" ++ msg ++ "\""
+
+notifySend :: [String] -> X ()
+notifySend xs = spawn $ "notify-send " ++ foldr1 catWithStyles xs
+  where
+    catWithStyles = printf "'%s' '%s'"
+
 
 -- My configurations
 
@@ -54,7 +78,7 @@ myStartupHook :: X ()
 myStartupHook = do
   spawnOnce "fcitx"
   spawnOnce "xfce4-clipman"
-  spawnOnce firstTerminal
+  spawnOnce "xfce4-terminal -e tmux"
 
 
 myWorkspaces :: [String]
@@ -79,11 +103,17 @@ myKeymappings =
   , ((superMask, xK_f), spawn "firefox")
   , ((superMask, xK_r), spawn "dmenu_run")
   , ((superMask, xK_m), spawn "xfce4-mixer")
-  , ((noModMask, xK_Print), spawn "import -window root ~/Picture/ScreenShot-$(date +'%Y-%m-%d-%H-%M-%S').png")  --TODO: notify to xmobar
-  , ((shiftMask, xK_Print), spawn "import -window $(xdotool getwindowfocus -f) ~/Picture/ScreenShot-$(date +'%Y-%m-%d-%H-%M-%S').png")
+  , ((noModMask, xK_Print), takeScreenShotFull)  --TODO: notify to xmobar
+  , ((shiftMask, xK_Print), takeScreenShotWindow)
   ]
   where
     cycleWindowsForward  = windows focusDown
     cycleWindowsBackward = windows focusUp
     swapNextWindow       = windows swapDown
     swapPrevWindow       = windows swapUp
+    takeScreenShotFull   = do
+      spawn "import -window root ~/Picture/ScreenShot-$(date +'%Y-%m-%d-%H-%M-%S').png"
+      espeak "shot the area"
+    takeScreenShotWindow = do
+      spawn "import -window $(xdotool getwindowfocus -f) ~/Picture/ScreenShot-$(date +'%Y-%m-%d-%H-%M-%S').png"
+      espeak "shot the window"
