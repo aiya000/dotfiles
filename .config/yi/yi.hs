f81ce548aad2d1fe4d2b26d1692e68adaf98a886
aiya000 <aiya000.develop@gmail.com>
2016-10-22 14:17:06 +0900

    Improve yi.hs for basic vim like config

--- .config/yi/yi.hs
+++ .config/yi/yi.hs
@@ -1,6 +1,48 @@
-import Yi (yi)
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Monoid ((<>))
+import Data.Prototype (override)
+import Yi.Boot (yi)
+import Yi.Config (Config(defaultKm))
 import Yi.Config.Default (defaultVimConfig)
+import Yi.Config.Simple (EditorM)
+import Yi.Event (Event(Event), Key(KASCII), Modifier(MCtrl))
+import Yi.Keymap (KeymapSet)
+import Yi.Keymap.Vim (mkKeymapSet, defVimConfig, vimBindings, VimBinding)
+import Yi.Keymap.Vim.Common (VimMode(Normal), RepeatToken(Drop))
+import Yi.Keymap.Vim.Utils (mkBindingE)
+import qualified Yi.Config.Simple as S
 
 
 main :: IO ()
 main = yi defaultVimConfig
+  { defaultKm = myDefaultKm
+  }
+
+-- Compose yi's vim keymapping and my keymapping
+myDefaultKm :: KeymapSet
+myDefaultKm = mkKeymapSet $ defVimConfig `override` \super _ -> super
+  { vimBindings = myBindings <> vimBindings super
+  }
+
+-- My keymapping
+myBindings :: [VimBinding]
+myBindings =
+  [ nmap (c 'p') S.previousTabE
+  , nmap (c 'n') S.nextTabE
+  --, nmap (map KASCII "gH") S.newTabE
+  --, nmap "<Home>" (withCurrentBuffer moveToSol)
+  --, nmap "<End>" (withCurrentBuffer moveToEol)
+  --, nmap " " (eval ":nohlsearch<CR>")
+  ]
+  where
+    -- like Vim's <C-x> key
+    c x = Event (KASCII x) [MCtrl]
+
+    nmap :: Event -> EditorM () -> VimBinding
+    nmap key x = mkBindingE Normal Drop (key, x, id)
+    --imap x y = V2.VimBindingE (\evs state -> case V2.vsMode state of
+    --                            V2.Insert _ ->
+    --                                fmap (const (y >> return V2.Continue))
+    --                                     (evs `V2.matchesString` x)
+    --                            _ -> V2.NoMatch)
