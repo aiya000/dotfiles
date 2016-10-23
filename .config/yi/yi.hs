6925e4d0630be5308e24cbe2e2cae14a712530ad
aiya000 <aiya000.develop@gmail.com>
2016-10-24 01:23:29 +0900

    Implement nmap' in yi.hs

--- .config/yi/yi.hs
+++ .config/yi/yi.hs
@@ -9,8 +9,9 @@ import Yi.Config.Simple (EditorM)
 import Yi.Event (Event(Event), Key(KASCII), Modifier(MCtrl))
 import Yi.Keymap (KeymapSet)
 import Yi.Keymap.Vim (mkKeymapSet, defVimConfig, vimBindings, VimBinding)
+import Yi.Keymap.Vim.Common (EventString)
 import Yi.Keymap.Vim.Common (VimMode(Normal), RepeatToken(Drop))
-import Yi.Keymap.Vim.Utils (mkBindingE)
+import Yi.Keymap.Vim.Utils (mkBindingE, mkStringBindingE)
 import qualified Yi.Config.Simple as S
 
 
@@ -28,19 +29,24 @@ myDefaultKm = mkKeymapSet $ defVimConfig `override` \super _ -> super
 -- My keymapping
 myBindings :: [VimBinding]
 myBindings =
-  [ nmap (c 'p') S.previousTabE
-  , nmap (c 'n') S.nextTabE
-  --, nmap (map KASCII "gH") S.newTabE
+  [ nmap  (c 'p') S.previousTabE
+  , nmap  (c 'n') S.nextTabE
+  , nmap' "gH"    S.newTabE
   --, nmap "<Home>" (withCurrentBuffer moveToSol)
   --, nmap "<End>" (withCurrentBuffer moveToEol)
   --, nmap " " (eval ":nohlsearch<CR>")
   ]
   where
-    -- like Vim's <C-x> key
+    -- like <C-x> key of Vim
     c x = Event (KASCII x) [MCtrl]
 
+    -- like nmap command of Vim
     nmap :: Event -> EditorM () -> VimBinding
     nmap key x = mkBindingE Normal Drop (key, x, id)
+
+    -- like nmap command of Vim from EventString
+    nmap' :: EventString -> EditorM () -> VimBinding
+    nmap' key x = mkStringBindingE Normal Drop (key, x, id)
     --imap x y = V2.VimBindingE (\evs state -> case V2.vsMode state of
     --                            V2.Insert _ ->
     --                                fmap (const (y >> return V2.Continue))
