6c747d1ea47aa88e3fb663ded0fbffedd3920171
aiya000 <aiya000.develop@gmail.com>
2016-11-06 00:32:42 +0900

    Remove .noremap and rename instead .noremap to it

--- .config/yi/Main.hs
+++ .config/yi/Main.hs
@@ -86,72 +86,71 @@ myBindings eval = normalBindings eval ++ insertBindings ++ visualBindings ++ exB
 -- Keymappings for V.VimMode V.Normal
 normalBindings :: VimEvaluator -> [V.VimBinding]
 normalBindings _ =
-  [ nnoremapE (keyC 'p') E.previousTabE
-  , nnoremapE (keyC 'n') E.nextTabE
-  , nnoremapY (keyC 'l') (refreshEditor >> printMsg "refreshed")
-  , nnoremapE' " h"  E.prevWinE  -- temporary
-  , nnoremapE' " j"  E.nextWinE  -- temporary
-  , nnoremapE' " k"  E.prevWinE  -- temporary
-  , nnoremapE' " l"  E.nextWinE  -- temporary
-  , nnoremapE' "gH"  E.newTabE
-  , nnoremapE' "ghh" E.newTabE  -- temporary
-  , nnoremapY' "ghq" closeWinOrQuitEditor
-  , nnoremapY' "ghQ" quitEditorWithBufferCheck
-  , nnoremapE' "ghc" E.closeBufferAndWindowE
-  , nnoremapE' "ghv" vsplit
-  --, nnoremapE' "ghs" (E.splitE >> ?)  -- Clone win to under
-  , nnoremapE' "gho" E.closeOtherE  -- Do :only
-  --, nnoremapE' "g:"  (eval ":buffers<CR>")  --FIXME: doesn't works
-  , nnoremapE' "gh\"" (resizeCurrentWin 3)
-  , nnoremapY' "<C-k><C-r>" reload
-  , nnoremapY' "<C-k><CR>"  viWrite  -- Yi interprets <C-j> as <CR>
-  , nnoremapY' "<C-k>J"     (fwriteAllY >> return ())
-  , nnoremapY' "\\e"        (withEditor vsplit    >> dired)
-  , nnoremapY' "\\\\E"      (withEditor E.newTabE >> dired)
-  --, nnoremapE' "<C-k><C-l>" (eval ":nohlsearch<CR>")  --FIXME: doesn't works correctly
-  , nnoremapY' "<CR>" (withCurrentBuffer $ B.lineDown >> B.newlineB >> B.lineUp)  -- insert newline to under
+  [ nnoremapE "<C-p>" E.previousTabE
+  , nnoremapE "<C-n>" E.nextTabE
+  , nnoremapY "<C-l>" (refreshEditor >> printMsg "refreshed")
+  , nnoremapE " h"  E.prevWinE  -- temporary
+  , nnoremapE " j"  E.nextWinE  -- temporary
+  , nnoremapE " k"  E.prevWinE  -- temporary
+  , nnoremapE " l"  E.nextWinE  -- temporary
+  --, nnoremapE "g:"  (eval ":buffers<CR>")  --FIXME: doesn't works
+  --, nnoremapE "g*"  staySearch -- TODO
+  , nnoremapE "gH"  E.newTabE
+  , nnoremapE "ghh" E.newTabE  -- temporary
+  , nnoremapY "ghq" closeWinOrQuitEditor
+  , nnoremapY "ghQ" quitEditorWithBufferCheck
+  , nnoremapE "ghc" E.closeBufferAndWindowE
+  , nnoremapE "ghv" vsplit
+  --, nnoremapE "ghs" (E.splitE >> ?)  -- Clone win to under
+  , nnoremapE "gho" E.closeOtherE  -- Do :only
+  , nnoremapE "gh\"" (resizeCurrentWin 3)
+  , nnoremapY "<C-k><C-r>" reload
+  , nnoremapY "<C-k><CR>"  viWrite  -- Yi interprets <C-j> as <CR>
+  , nnoremapY "<C-k>J"     (fwriteAllY >> return ())
+  , nnoremapY "\\e"        (withEditor vsplit    >> dired)
+  , nnoremapY "\\\\E"      (withEditor E.newTabE >> dired)
+  --, nnoremapE "<C-k><C-l>" (eval ":nohlsearch<CR>")  --FIXME: doesn't works correctly
+  , nnoremapY "<CR>" (withCurrentBuffer $ B.lineDown >> B.newlineB >> B.lineUp)  -- insert newline to under
   -- Override default
   --TODO: implement
-  --, nnoremapE' ">>" tabspaceNum
-  --, nnoremapE' "<<" tabspaceNum
+  --, nnoremapE ">>" tabspaceNum
+  --, nnoremapE "<<" tabspaceNum
   -- Complete official lost things
-  , nnoremapE' "<C-w>w" E.nextWinE
-  , nnoremapE' "gd"     searchFromHead
+  , nnoremapE "<C-w>w" E.nextWinE
+  , nnoremapE "gd"     searchFromHead
   ]
   where
     -- Like nnoremap of Vim for EditorM
-    nnoremapE :: Event -> EditorM () -> V.VimBinding
-    nnoremapE key x = nnoremapE' (eventToEventString key) x
-    -- Like nnoremap of Vim for EditorM from V.EventString
-    nnoremapE' :: V.EventString -> EditorM () -> V.VimBinding
-    nnoremapE' key x = mkStringBindingE V.Normal V.Drop (key, x, id)
+    nnoremapE :: V.EventString -> EditorM () -> V.VimBinding
+    nnoremapE key x = mkStringBindingE V.Normal V.Drop (key, x, id)
     -- for YiM
-    nnoremapY  :: Event -> YiM () -> V.VimBinding
-    nnoremapY  key x = nnoremapY' (eventToEventString key) x
-    nnoremapY' :: V.EventString -> YiM () -> V.VimBinding
-    nnoremapY' key x = mkStringBindingY V.Normal (key, x, id)
+    nnoremapY :: V.EventString -> YiM () -> V.VimBinding
+    nnoremapY key x = mkStringBindingY V.Normal (key, x, id)
 
     resizeCurrentWin :: Int -> EditorM ()
     resizeCurrentWin lineNum = undefined
-    -- Clone a window to right, this means default behavior of Vim :vsplit
-    vsplit = E.splitE >> E.prevWinE
     -- normal gd of Vim
+    searchFromHead :: EditorM ()
     searchFromHead = do
       word <- toString <$> withCurrentBuffer readCurrentWordB
       withCurrentBuffer $  B.moveToLineColB 0 0
       doSearch (Just word) [IgnoreCase] Forward
       return ()
+    --
+    staySearch = undefined
+    -- Clone a window to right, this means default behavior of Vim :vsplit
+    vsplit = E.splitE >> E.prevWinE
 
 
 -- Keymappings for V.VimMode (∀a. V.Insert a)
 insertBindings :: [V.VimBinding]
 insertBindings =
-  [ inoremapE (keyC 'l') (switchModeE V.Normal >> withCurrentBuffer B.leftB)  -- <Esc> behavior of Vim
+  [ inoremapE "<C-l>" (switchModeE V.Normal >> withCurrentBuffer B.leftB)  -- <Esc> behavior of Vim
   --FIXME: cannot unset modified flag
-  , inoremapY' "<C-k><CR>" (viWrite >> switchModeY V.Normal)  -- Yi interprets <C-j> as <CR>
-  , inoremapY' "<C-k><C-k>" (killLine Nothing)
+  , inoremapY "<C-k><CR>" (viWrite >> switchModeY V.Normal)  -- Yi interprets <C-j> as <CR>
+  , inoremapY "<C-k><C-k>" (killLine Nothing)
   -- Override default
-  , inoremapY' "<Tab>" (withCurrentBuffer $ B.insertN . fromString $ replicate tabspaceNum ' ')  --NOTE: Does Yi has :set ts=n like stateful function ?
+  , inoremapY "<Tab>" (withCurrentBuffer $ B.insertN . fromString $ replicate tabspaceNum ' ')  --NOTE: Does Yi has :set ts=n like stateful function ?
   ]
   where
     -- The keymapping implementor for both of V.VimBindingY and V.VimBindingE
@@ -160,22 +159,19 @@ insertBindings =
       case V.vsMode state of
         V.Insert _ -> (const $ context >> return V.Continue) <$> key' `V.matchesString` key
         _          -> V.NoMatch
-    -- Like inoremap of Vim from Event for EditorM
-    inoremapE :: Event -> EditorM () -> V.VimBinding
-    inoremapE key x = inoremapE' (eventToEventString key) x
-    -- Like inoremap of Vim from V.EventString for EditorM
+    -- Like inoremap of Vim for EditorM
     -- for ∀a. V.Insert a
-    inoremapE' :: V.EventString -> EditorM () -> V.VimBinding
-    inoremapE' key x = V.VimBindingE $ implBinding key x
-    -- Like inoremap of Vim from V.EventString for YiM
+    inoremapE :: V.EventString -> EditorM () -> V.VimBinding
+    inoremapE key x = V.VimBindingE $ implBinding key x
+    -- Like inoremap of Vim for YiM
     -- for ∀a. V.Insert a
-    inoremapY' :: V.EventString -> YiM () -> V.VimBinding
-    inoremapY' key x = V.VimBindingY $ implBinding key x
+    inoremapY :: V.EventString -> YiM () -> V.VimBinding
+    inoremapY key x = V.VimBindingY $ implBinding key x
 
 -- Keymapping for V.VimMode (∀a V.Visual a)
 visualBindings :: [V.VimBinding]
 visualBindings =
-  [ vnoremapE (keyC 'l') exitVisual
+  [ vnoremapE "<C-l>" exitVisual
   ]
   where
     -- The keymappings implementor for both of V.VimBindingY and V.VimBindingE
@@ -185,11 +181,8 @@ visualBindings =
         V.Visual _ -> (const $ context >> return V.Continue) <$> key' `V.matchesString` key
         _          -> V.NoMatch
     -- Like vnoremap of Vim for EditorM
-    vnoremapE :: Event -> EditorM () -> V.VimBinding
-    vnoremapE key x = vnoremapE' (eventToEventString key) x
-    -- Like vnoremap of Vim for EditorM from V.EventString
-    vnoremapE' :: V.EventString -> EditorM () -> V.VimBinding
-    vnoremapE' key x = V.VimBindingE $ implBinding key x
+    vnoremapE :: V.EventString -> EditorM () -> V.VimBinding
+    vnoremapE key x = V.VimBindingE $ implBinding key x
 
     -- See https://www.stackage.org/haddock/lts-7.4/yi-0.12.6/src/Yi.Keymap.Vim.ExMap.html#exitEx
     exitVisual = do
@@ -200,15 +193,12 @@ visualBindings =
 -- Keymappings for V.VimMode V.Ex
 exBindings :: [V.VimBinding]
 exBindings =
-  [ cnoremap (keyC 'l') exitEx'
+  [ cnoremapE "<C-l>" exitEx'
   ]
   where
     -- Like cnoremap of Vim
-    cnoremap :: Event -> EditorM () -> V.VimBinding
-    cnoremap key x = cnoremap' (eventToEventString key) x
-    -- Like cnoremap of Vim from V.EventString
-    cnoremap' :: V.EventString -> EditorM () -> V.VimBinding
-    cnoremap' key x = mkStringBindingE V.Ex V.Finish (key, x, id)
+    cnoremapE :: V.EventString -> EditorM () -> V.VimBinding
+    cnoremapE key x = mkStringBindingE V.Ex V.Finish (key, x, id)
 
     -- See https://www.stackage.org/haddock/lts-7.4/yi-0.12.6/src/Yi.Keymap.Vim.ExMap.html#exitEx
     exitEx' = do
