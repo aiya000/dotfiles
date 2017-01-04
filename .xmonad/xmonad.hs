19b3a35399106b9b1b637660bac968afb8163c54
aiya000 <aiya000.develop@gmail.com>
2017-01-04 18:38:14 +0900

    Remove 'baseKeys' $ xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -78,45 +78,40 @@ myWorkspaces :: [String]
 myWorkspaces = map show [1..4]
 
 
-baseKeys :: KeyMask -> [((KeyMask, KeySym), X ())]
-baseKeys casualMask =
-  [ ((casualMask, xK_h), windows focusUp)
-  , ((casualMask, xK_l), windows focusDown)
-  , ((casualMask, xK_j), withFocused (sendMessage . MergeAll))
-  , ((casualMask, xK_k), withFocused (sendMessage . UnMerge))
-  , ((superMask, xK_l), withFocused $ keysMoveWindow (5,0))
-  , ((superMask, xK_h), withFocused $ keysMoveWindow (-5,0))
-  , ((superMask, xK_j), withFocused $ keysMoveWindow (0,5))
-  , ((superMask, xK_k), withFocused $ keysMoveWindow (0,-5))
+myNormalKeys :: [((KeyMask, KeySym), X ())]
+myNormalKeys =
+  [ ((altMask .|. controlMask, xK_t), spawn "termite")
+  , ((altMask .|. shiftMask, xK_h), windows swapUp)
+  , ((altMask .|. shiftMask, xK_i), nextScreen)
+  , ((altMask .|. shiftMask, xK_l), windows swapDown)
+  , ((altMask, xK_h), windows focusUp)
+  , ((altMask, xK_j), withFocused (sendMessage . MergeAll))
+  , ((altMask, xK_k), withFocused (sendMessage . UnMerge))
+  , ((altMask, xK_l), windows focusDown)
+  , ((altMask, xK_x), xmonadSwitchKeyModeToHHKB)
+  , ((noModMask, xK_Print), takeScreenShot FullScreen)
+  , ((shiftMask, xK_Print), takeScreenShot ActiveWindow)
+  , ((superMask .|. shiftMask, xK_F1), spawn "xscreensaver-command -lock; sudo pm-hibernate") -- ^ must add pm-hibernate to sudoers without inputting password
+  , ((superMask .|. shiftMask, xK_a), sinkAll)
   , ((superMask .|. shiftMask, xK_i), sendMessage NextLayout)
   , ((superMask, xK_F1),  spawn "xscreensaver-command -lock; sudo pm-suspend") -- ^ must add pm-suspend to sudoers without inputting password
+  , ((superMask, xK_F12), spawn "xscreensaver-command -lock")
   , ((superMask, xK_F4),  spawn "light -U 10")
   , ((superMask, xK_F5),  spawn "light -A 10")
   , ((superMask, xK_F6),  void $ toggleMute)
   , ((superMask, xK_F7),  void $ lowerVolume 5)
   , ((superMask, xK_F8),  void $ raiseVolume 5)
-  , ((superMask, xK_F12), spawn "xscreensaver-command -lock")
-  , ((superMask .|. shiftMask, xK_F1), spawn "xscreensaver-command -lock; sudo pm-hibernate") -- ^ must add pm-hibernate to sudoers without inputting password
-  ]
-
-myNormalKeys :: [((KeyMask, KeySym), X ())]
-myNormalKeys =
-  [ ((altMask .|. shiftMask, xK_l), windows swapDown)
-  , ((altMask .|. shiftMask, xK_h), windows swapUp)
-  , ((altMask .|. shiftMask, xK_i), nextScreen)
-  , ((superMask .|. shiftMask, xK_a), sinkAll)
-  , ((altMask .|. controlMask, xK_t), spawn "termite")
   , ((superMask, xK_e), spawn "thunar")
-  , ((superMask, xK_r), spawn "dmenu_run")
   , ((superMask, xK_f), spawn "firefox")
+  , ((superMask, xK_h), withFocused $ keysMoveWindow (-5,0))
+  , ((superMask, xK_j), withFocused $ keysMoveWindow (0,5))
+  , ((superMask, xK_k), withFocused $ keysMoveWindow (0,-5))
+  , ((superMask, xK_l), withFocused $ keysMoveWindow (5,0))
   , ((superMask, xK_m), spawn "xfce4-mixer")
-  , ((noModMask, xK_Print), takeScreenShot FullScreen)
-  , ((shiftMask, xK_Print), takeScreenShot ActiveWindow)
-  , ((hhkbCasualMask, xK_x), xmonadSwitchKeyModeToHHKB)
+  , ((superMask, xK_r), spawn "dmenu_run")
   ]
   ++ [ ((altMask .|. shiftMask, numKey), moveWindowTo myWorkspaces workspace)
       | (numKey, workspace) <- zip [xK_1 .. xK_9] . map S $ [1 .. length myWorkspaces] ]
-  ++ baseKeys altMask
   where
     xmonadSwitchKeyModeToHHKB :: X ()
     xmonadSwitchKeyModeToHHKB = touch hhkbKeyModeFlagFile >> xmonadRestartWithMessage
@@ -124,25 +119,29 @@ myNormalKeys =
 
 myHHKBKeys :: [((KeyMask, KeySym), X ())]
 myHHKBKeys =
-  [ ((hhkbCasualMask .|. altMask, xK_l), windows swapDown)
-  , ((hhkbCasualMask .|. altMask, xK_h), windows swapUp)
-  , ((hhkbCasualMask, xK_i), nextScreen)
-  , ((hhkbCasualMask, xK_c), kill)
+  [ ((hhkbCasualMask .|. altMask, xK_h), windows swapUp)
+  , ((hhkbCasualMask .|. altMask, xK_l), windows swapDown)
   , ((hhkbCasualMask, xK_a), sinkAll)
-  , ((hhkbCasualMask, xK_t), spawn "termite")
+  , ((hhkbCasualMask, xK_c), kill)
   , ((hhkbCasualMask, xK_e), spawn "thunar")
-  , ((hhkbCasualMask, xK_r), spawn "dmenu_run")
   , ((hhkbCasualMask, xK_f), spawn "firefox")
+  , ((hhkbCasualMask, xK_h), windows focusUp)
+  , ((hhkbCasualMask, xK_i), nextScreen)
+  , ((hhkbCasualMask, xK_j), withFocused (sendMessage . MergeAll))
+  , ((hhkbCasualMask, xK_k), withFocused (sendMessage . UnMerge))
+  , ((hhkbCasualMask, xK_l), windows focusDown)
   , ((hhkbCasualMask, xK_m), spawn "xfce4-mixer")
+  , ((hhkbCasualMask, xK_r), spawn "dmenu_run")
+  , ((hhkbCasualMask, xK_t), spawn "termite")
   , ((hhkbCasualMask, xK_x), xmonadSwitchKeyModeToNormal)
   , ((noModMask, xK_Print), takeScreenShot FullScreen)
   , ((shiftMask, xK_Print), takeScreenShot ActiveWindow)
+  , ((superMask .|. shiftMask, xK_i), sendMessage NextLayout)
   ]
   ++ [ ((hhkbCasualMask, numKey), moveWindowTo myWorkspaces workspace)
       | (numKey, workspace) <- zip [xK_1 .. xK_9] . map S $ [1 .. length myWorkspaces] ]
   ++ [ ((hhkbCasualMask, numKey), windows . greedyView $ workspace)
        | (numKey, workspace) <- zip [xK_1 .. xK_9] myWorkspaces ]
-  ++ baseKeys hhkbCasualMask
   where
     xmonadSwitchKeyModeToNormal :: X ()
     xmonadSwitchKeyModeToNormal = (liftIO $ removeFile hhkbKeyModeFlagFile) >> xmonadRestartWithMessage
