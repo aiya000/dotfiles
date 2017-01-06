38c93167bf19fc434b198d119d020b89bf1773fb
aiya000 <aiya000.develop@gmail.com>
2017-01-07 00:45:02 +0900

    Modify xmonadRestartWithMessage

--- .xmonad/XMonadConfig/CommandWrapper.hs
+++ .xmonad/XMonadConfig/CommandWrapper.hs
@@ -43,7 +43,4 @@ touch = liftIO . flip writeFile ""
 -- xmonadRestartWithMessage depends haskell-stack, sleep and notify-send
 xmonadRestartWithMessage :: X ()
 xmonadRestartWithMessage =
-  spawn $ "xmonad --recompile && " ++
-          "xmonad --restart && " ++
-          "sleep 1 && " ++
-          "notify-send 'XMonad' 'Restarted'"
+  spawn $ "xmonad-config --restart && notify-send 'XMonad' 'Restarted'"
