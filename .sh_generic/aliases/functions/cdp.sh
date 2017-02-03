8a588825866ea76adcea25cb6847ea86f5a2628f
aiya000 <aiya000.develop@gmail.com>
2017-02-03 12:47:45 +0900

    Fix global variable pollution by cdp

--- .sh_generic/aliases/functions/cdp.sh
+++ .sh_generic/aliases/functions/cdp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 function cdp () {
-	depth=$([ -n "$1" ] && echo "$1" || echo 3)
+	local depth=$([ -n "$1" ] && echo "$1" || echo 3)
 
 	# Sort by the depth of the path
 	find -maxdepth $depth -type d | awk '{print $1 " / " gsub("/", $1)}' | sed -r 's/(.*) \/ (.*)/\2 \/ \1/g' | sort -n | sed -r 's/(.*) \/ (.*)/\2/g' | eval "fzf $FZF_CASUAL_OPTIONS" | read target_dir
