adac7c1cd31cff1beb812b3c7b015f3cac5ee985
aiya000 <aiya000.develop@gmail.com>
2017-02-03 12:46:14 +0900

    Revert "Enhance cdp performance but a dependenc...
    Revert "Enhance cdp performance but a dependency is added"
    
    This reverts commit 25bf5fb115d82fbc64ce32c705c93f093a489634.

--- .sh_generic/aliases/functions/cdp.sh
+++ .sh_generic/aliases/functions/cdp.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
 function cdp () {
-	local depth=$([ -n "$1" ] && echo "$1" || echo 5)
+	depth=$([ -n "$1" ] && echo "$1" || echo 3)
 
-	# 0. Show the directories of under the `pwd`
-	# 1. Ignore the files
-	# 2. Convert the full path to the relative path
-	# 3. Ignore it if its depth is over the $depth
-	# and select it
-	locate --null --regex "$(pwd)" \
-		| xargs --null sh -c 'for i do [ -d "$i" ] && printf "%s\n" "$i"; done' 2> /dev/null \
-		| cut -c $(pwd | wc -m | xargs -I _ expr _ + 1 )- \
-		| cut -d'/' -f "-$(( ${depth} + 1 ))" | uniq \
-		| eval "fzf $FZF_CASUAL_OPTIONS" \
-		| read target_dir
+	# Sort by the depth of the path
+	find -maxdepth $depth -type d | awk '{print $1 " / " gsub("/", $1)}' | sed -r 's/(.*) \/ (.*)/\2 \/ \1/g' | sort -n | sed -r 's/(.*) \/ (.*)/\2/g' | eval "fzf $FZF_CASUAL_OPTIONS" | read target_dir
 
 	if [ "$?" -eq 0 ] ; then
 		cd $target_dir
