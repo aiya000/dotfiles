7a1dac290b9c88064e3ac06ad9cb7ad7a8e75a1c
aiya000 <aiya000.develop@gmail.com>
2017-04-04 10:54:25 +0900

    Delete .xmonad

--- .xmonad/XMonadConfig/Types.hs
+++
@@ -1,8 +0,0 @@
-module XMonadConfig.Types
-  ( WorkspaceName
-  , Workspaces
-  ) where
-
--- | The another expression of XMonad.Config.XConfig.workspaces
-type Workspaces    = [WorkspaceName]
-type WorkspaceName = String
