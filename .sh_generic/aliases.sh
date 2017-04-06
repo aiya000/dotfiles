fb0d0dea0aea1707fb8a03bc7de8c65973de61cc
aiya000 <aiya000.develop@gmail.com>
2017-04-06 10:54:45 +0900

    Remove mount4u.ext[234] aliases

--- .sh_generic/aliases.sh
+++ .sh_generic/aliases.sh
@@ -87,9 +87,6 @@ alias x=xdg-open
 alias date-simple='date +"%Y-%m-%d"'
 alias mount4u.ntfs="sudo mount -o user=$(whoami),uid=$(id -u),gid=$(id -g),iocharset=utf8"
 alias mount4u.vfat=mount4u.ntfs
-alias mount4u.ext2="sudo mount -o iocharset=utf8"
-alias mount4u.ext3=mount4u.ext2
-alias mount4u.ext4=mount4u.ext2
 
 i_have docker && alias docker-rm-all-containers='sudo docker rm `sudo docker ps -a -q`'
 i_have rsync  && alias cp-with-progress='rsync --partial --progress'
