e2d980cc6bb079aa587dd0da855f6aa696abd50e
aiya000 <aiya000.develop@gmail.com>
2017-08-28 18:04:22 +0900

    Add sudo alias to cygwin.sh

--- .sh_generic/cygwin.sh
+++ .sh_generic/cygwin.sh
@@ -11,3 +11,14 @@ alias_of netstat='cocot netstat'
 alias_of nslookup='cocot nslookup'
 alias_of updatedb='updatedb --localpaths="/bin /dev /etc /home /lib /usr /var /opt" --prunepaths="/usr/tmp /var/tmp"'
 alias_of mysql='mysql --pager="less -r -S -n -i -F -X" --protocol=TCP'
+
+# Emulate sudo (See http://qiita.com/AinoMegumi/items/fd56711fe1fd2a0e1bbf)
+function sudo () {
+    which cocot > /dev/null 2>&1
+    if [ "$?" -eq 0 ] ; then
+        cygwin_sudo_cocot=cocot
+    else
+        cygwin_sudo_cocot=''
+    fi
+    $cygwin_sudo_cocot powershell Start-Process "$1" -Verb runas
+}
