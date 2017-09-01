d51011d725558aa5b56ffaace02005a020790496
aiya000 <aiya000.develop@gmail.com>
2017-09-01 11:27:16 +0900

    Change indentation

--- .sh_generic/completion.sh
+++ .sh_generic/completion.sh
@@ -2,20 +2,20 @@
 
 # Use git-completion
 if [ -f /usr/share/git/completion/git-completion.zsh ] && [ ! -f "$ZDOTDIR/_git" ] ; then
-	cp /usr/share/git/completion/git-completion.zsh "$ZDOTDIR/_git"
+    cp /usr/share/git/completion/git-completion.zsh "$ZDOTDIR/_git"
 fi
 
 # Use travis-completion
 if [ -f ~/.travis/travis.sh ] ; then
-	# shellcheck disable=SC1090
-	source ~/.travis/travis.sh
+    # shellcheck disable=SC1090
+    source ~/.travis/travis.sh
 fi
 
 # Use stack-completion
 type stack > /dev/null 2>&1
 if [ "$?" -eq 0 ] ; then
-	# This completion needs compinit and bashcompinit function
-	# > autoload -U compinit     && compinit
-	# > autoload -U bashcompinit && bashcompinit
-	eval "$(stack --bash-completion-script stack)"
+    # This completion needs compinit and bashcompinit function
+    # > autoload -U compinit     && compinit
+    # > autoload -U bashcompinit && bashcompinit
+    eval "$(stack --bash-completion-script stack)"
 fi
