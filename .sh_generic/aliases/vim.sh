fd0b3e3aabed90f8c5b9f800b4efe351f778ee3d
aiya000 <aiya000.develop@gmail.com>
2017-09-23 23:38:47 +0900

    Change indentation

--- .sh_generic/aliases/vim.sh
+++ .sh_generic/aliases/vim.sh
@@ -2,46 +2,45 @@
 # This makes the aliases with both vim and neovim
 
 function dotfile_config () {
-	if [ -f "${HOME}/.dotfiles/${1}" ] ; then
-		nvim "${HOME}/.dotfiles/${1}"
-	else
-		nvim "${HOME}/${1}"
-	fi
+    if [ -f "${HOME}/.dotfiles/${1}" ] ; then
+        nvim "${HOME}/.dotfiles/${1}"
+    else
+        nvim "${HOME}/${1}"
+    fi
 }
 
 # Vim
 alias vi='vim -u NONE --noplugin'
 alias vime='vim -c ":bufdo tab split" +q'
 function vim-session () {
-	vim -c "UniteSessionLoad ${1}"
+    vim -c "UniteSessionLoad ${1}"
 }
 
 
 # NeoVim
 alias nvime='nvim -c ":bufdo tab split" +q'
 function nvim-session () {
-	nvim -c "UniteSessionLoad ${1}"
+    nvim -c "UniteSessionLoad ${1}"
 }
 
 # :SessionSaveInGitBranch compatibled command. (it is defined in dotfiles/.vim/plugin/vimrc.vim)
 # Open the nvim session which is associated the current git branch
 function nvim-current-session () {
-	local branch
-	local repo_name
-	local session_name
+    local repo_name
+    local session_name
 
-	branch_name="$(git branch 2> /dev/null | sort | tail -1 | awk '{print $2}')"
-	# shellcheck disable=SC2181
-	if [ "$?" -ne 0 ] ; then
-		echo 'Is here the git branch ?' > /dev/stderr
-		exit
-	fi
+    branch_name="$(git branch 2> /dev/null | sort | tail -1 | awk '{print $2}')"
+    # shellcheck disable=SC2181
+    if [ "$?" -ne 0 ] ; then
+        echo 'Is here the git branch ?' > /dev/stderr
+        exit
+    fi
 
-	repo_name="$(git rev-parse --show-toplevel | sed -r 's;.*/(.*);\1;')"
-	branch_name_="$(echo "$branch_name" | sed -r 's;/;-;g')"
+    repo_name="$(git rev-parse --show-toplevel | sed -r 's;.*/(.*);\1;')"
+    branch_name_="$(echo "$branch_name" | sed -r 's;/;-;g')"
 
-	session_name="${repo_name}-${branch_name_}.vim"
-	nvim-session "$session_name"
+    session_name="${repo_name}-${branch_name_}.vim"
+    nvim-session "$session_name"
 }
 
 alias nvimdiff='\nvim -d'
@@ -49,7 +48,7 @@ alias nvimdiff='\nvim -d'
 alias vimless='nvim - -c "setl buftype=nofile nolist | nnoremap <buffer> Q :<C-u>q<CR>"'
 alias vimls='nvim -c "read! ls" -c "nnoremap <buffer> Q :<C-u>q<CR> | setl nolist buftype=nofile | normal! Gddgg"'
 function vimman () {
-	nvim -c "Man ${1}" +only
+    nvim -c "Man ${1}" +only
 }
 
 alias vimgs='nvim +"Gina status" +only'
@@ -59,7 +58,7 @@ alias vimshell='nvim +VimShell'
 alias vimconsole='nvim +VimConsoleOpen'
 alias adrone='nvim +AdroneHome'
 function lingr () {
-	nvim -c "J6uil ${1}"
+    nvim -c "J6uil ${1}"
 }
 
 ## Twitter
@@ -69,18 +68,18 @@ alias twitter-public='nvim +TwitterPublic'
 alias tweet-public='nvim +TweetPublic'
 
 function twitter-usertimeline() {
-	nvim -c "TweetVimUserTimeline ${1}"
+    nvim -c "TweetVimUserTimeline ${1}"
 }
 
 function tweet-say() {
-	yes | vim -c "TweetVimCommandSay ${1}" +q
+    yes | vim -c "TweetVimCommandSay ${1}" +q
 }
 
 function tweet-public-say() {
-	yes | vim \
-		-c "TweetVimSwitchAccount public_ai000ya" \
-		-c "TweetVimCommandSay ${1}" \
-		+q
+    yes | vim \
+        -c "TweetVimSwitchAccount public_ai000ya" \
+        -c "TweetVimCommandSay ${1}" \
+        +q
 }
 
 ## :terminal
