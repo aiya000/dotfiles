564b944a3cb639c7b1f3bf504d5da17fe3137633
aiya000 <aiya000.develop@gmail.com>
2017-09-27 15:26:30 +0900

    Change indentation

--- .sh_generic/premise.sh
+++ .sh_generic/premise.sh
@@ -6,16 +6,16 @@
 #NOTE: I should rename this to have_i()
 # If I have it command, return 0. otherwise return 1.
 function i_have () {
-	which "$1" > /dev/null 2>&1
+    which "$1" > /dev/null 2>&1
 }
 
 # If I have a command what it is specified same as alias name,
 # define the alias.
 # otherwise don't define it.
 function alias_of () {
-	alias_detail="$1"
-	name="`echo $alias_detail | awk -F = '{print $1}'`"
-	i_have "$name" && alias "$alias_detail"
+    alias_detail="$1"
+    name="`echo $alias_detail | awk -F = '{print $1}'`"
+    i_have "$name" && alias "$alias_detail"
 }
 
 
