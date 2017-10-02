e78ac9559d3945daef4573888633e2401734cbdc
aiya000 <aiya000.develop@gmail.com>
2017-10-02 19:10:42 +0900

    Add ps alias for cygwin

--- .sh_generic/cygwin.sh
+++ .sh_generic/cygwin.sh
@@ -11,6 +11,7 @@ alias_of netstat='cocot netstat'
 alias_of nslookup='cocot nslookup'
 alias_of updatedb='updatedb --localpaths="/bin /dev /etc /home /lib /usr /var /opt" --prunepaths="/usr/tmp /var/tmp"'
 alias_of mysql='mysql --pager="less -r -S -n -i -F -X" --protocol=TCP'
+alias_of ps='ps -W'
 
 # Emulate sudo (See http://qiita.com/AinoMegumi/items/fd56711fe1fd2a0e1bbf)
 function sudo () {
