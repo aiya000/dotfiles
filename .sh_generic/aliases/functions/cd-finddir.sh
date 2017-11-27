be35a0f9e6cfb6baf17ed6d6e55387f207776b96
aiya000 <aiya000.develop@gmail.com>
2017-11-27 10:43:38 +0900

    Change indentation

--- .sh_generic/aliases/functions/cd-finddir.sh
+++ .sh_generic/aliases/functions/cd-finddir.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 function cd-finddir () {
-	local depth=$([ -n "$1" ] && echo "$1" || echo 3)
+    local depth=$([ -n "$1" ] && echo "$1" || echo 3)
 
-	# Sort by the depth of the path
-	find -maxdepth $depth -type d | awk '{print $1 " / " gsub("/", $1)}' | sed -r 's/(.*) \/ (.*)/\2 \/ \1/g' | sort -n | sed -r 's/(.*) \/ (.*)/\2/g' | fzf | read target_dir
+    # Sort by the depth of the path
+    find -maxdepth $depth -type d | awk '{print $1 " / " gsub("/", $1)}' | sed -r 's/(.*) \/ (.*)/\2 \/ \1/g' | sort -n | sed -r 's/(.*) \/ (.*)/\2/g' | fzf | read target_dir
 
-	if [ "$?" -eq 0 ] ; then
-		cd $target_dir
-	fi
+    if [ "$?" -eq 0 ] ; then
+    	cd $target_dir
+    fi
 }
