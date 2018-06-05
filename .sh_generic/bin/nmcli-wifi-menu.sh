146aa9157d593798ed9e785b9b0abda4eacd6866
aiya000 <aiya000.develop@gmail.com>
2018-06-05 11:41:30 +0900

    indent

--- .sh_generic/bin/nmcli-wifi-menu.sh
+++ .sh_generic/bin/nmcli-wifi-menu.sh
@@ -6,9 +6,9 @@ selectedItem=$(echo $list | tail -n $(expr $itemNum - 1) | fzf --exit-0 | sed 's
 
 # If nmcli couldn't find AP
 if [ "$?" -ne 0 -o "$selectedItem" = '' ] ; then
-	echo 'error :(' > /dev/stderr
-	echo 'You may interuppted fzf or hardware wifi switch is disabled' > /dev/stderr
-	exit 1
+    echo 'error :(' > /dev/stderr
+    echo 'You may interuppted fzf or hardware wifi switch is disabled' > /dev/stderr
+    exit 1
 fi
 
 # Extract values
@@ -17,9 +17,9 @@ passwordNeeded=$(echo $selectedItem | awk '{print $2}')
 
 # Require password if needed
 if [ "$passwordNeeded" != "--" ] ; then
-	echo -n "password: "
-	read -s password
-	passwordArgument="password $password"
+    echo -n "password: "
+    read -s password
+    passwordArgument="password $password"
 fi
 
 # Apply connecting to selected AP
