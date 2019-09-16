755d4871ca9772b2cd63da92b182ba9a23441705
aiya000 <aiya000.develop@gmail.com>
2019-09-16 13:41:47 +0900

    poi

---
+++ .screenlayout/casual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --primary --mode 2160x1440 --pos 0x517 --rotate right --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off --output DVI-I-1-1 --mode 2560x1080 --pos 1440x0 --rotate normal
