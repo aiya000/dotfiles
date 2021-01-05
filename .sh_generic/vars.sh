d00dbe918e572d41729630be6b6e334e15002d6a
aiya000 <aiya000.develop@gmail.com>
2021-01-05 15:53:27 +0900

    Improve dust

--- .sh_generic/vars.sh
+++ .sh_generic/vars.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-DUSTBOX=$HOME/.backup/dustbox/$(date +'%Y-%m-%d')
-export DUSTBOX
-
-VIM_SESSION=$HOME/.backup/vim-backup/session
-export VIM_SESSION
+export DUSTBOX=$HOME/.backup/dustbox
+export VIM_SESSION=$HOME/.backup/vim-backup/session
