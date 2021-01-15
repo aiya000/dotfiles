51a56675b765ecb75b8dcb96e10bc417ef4cb7b5
aiya000 <aiya000.develop@gmail.com>
2021-01-15 18:08:16 +0900

    aliases

--- .sh_generic/aliases.sh
+++ .sh_generic/aliases.sh
@@ -51,202 +51,218 @@ source ~/.sh_generic/aliases/vim.sh
 # Git {{{
 
 if i_have git ; then
-    git_taking_limit=100
-    alias _gr='git reset'
-    alias _grh='git reset --hard'
-    alias _grh~='git reset --hard HEAD~'
-    alias _grh~~='git reset --hard HEAD~~'
-    alias _grh~~~='git reset --hard HEAD~~~'
-    alias _grh~~~~='git reset --hard HEAD~~~~'
-    alias _grs='git reset --soft'
-    alias _grs~='git reset --soft HEAD~'
-    alias _grs~~='git reset --soft HEAD~~'
-    alias _grs~~~='git reset --soft HEAD~~~'
-    alias _grs~~~~='git reset --soft HEAD~~~~'
-    alias ga='git add'
-    alias gaa='git add -A'
-    alias gap='git add -p'
-    alias gb='git branch'
-    alias gbd='git branch --delete'
-    alias _gbd='git branch -D'
-    alias _gbdf='git branch --delete --force'
-    alias gbm='git branch -m'
-    alias gc='git commit --verbose'
-    alias gcam='git commit --verbose --amend'
-    alias gcm='git commit -m'
-    alias gcf='git commit --fixup'
-    alias gaacm='git add -A && git commit -m'
-    alias gaacamm='git add -A && git commit --amend -m'
-    alias gco='git checkout'
-    alias gsw='git switch'
-    alias gswc='git switch --create'
-    alias gswd='git switch --detach'
-    alias gres='git restore'
-    alias gd='git diff'
-    alias gdh='git diff HEAD~..HEAD'
-    alias gds='git diff --staged'
-    alias gdst0='git diff stash@{0}'
-    alias gdst1='git diff stash@{1}'
-    alias gdst2='git diff stash@{2}'
-    # shellcheck disable=SC2139
-    alias gl="git log --name-only -$git_taking_limit"
-    # shellcheck disable=SC2139
-    alias glo="git log --oneline -$git_taking_limit"
-    alias gll='glo | head'
-    # shellcheck disable=SC2139
-    alias glp="git log --patch -$git_taking_limit"
-    # shellcheck disable=SC2139
-    alias glf="git log --name-only -$git_taking_limit"
-    alias gmv='git mv'
-    alias gr='git rebase'
-    alias gra='git rebase --abort'
-    alias grc='git rebase --continue'
-    alias gri='git rebase --interactive --autosquash'
-    alias gri~='git rebase --interactive --autosquash HEAD~'
-    alias gri~~='git rebase --interactive --autosquash HEAD~~'
-    alias gri~~~='git rebase --interactive --autosquash HEAD~~~'
-    alias gri~~~~='git rebase --interactive --autosquash HEAD~~~~'
-    alias gri~~~~~='git rebase --interactive --autosquash HEAD~~~~~'
-    alias gri~~~~~~='git rebase --interactive --autosquash HEAD~~~~~~'
-    alias gri~~~~~~~='git rebase --interactive --autosquash HEAD~~~~~~~'
-    alias gri~~~~~~~~='git rebase --interactive --autosquash HEAD~~~~~~~~'
-    alias gri~~~~~~~~~='git rebase --interactive --autosquash HEAD~~~~~~~~~'
-    alias gri~~~~~~~~~~='git rebase --interactive --autosquash HEAD~~~~~~~~~~'
-    alias gr-onto='git rebase --onto'
-    alias grev='git revert'
-    alias grev~='git revert HEAD~'
-    alias grev~~='git revert HEAD~~'
-    alias grev~~~='git revert HEAD~~~'
-    alias grev~~~~='git revert HEAD~~~~'
-    alias grev~~~~~='git revert HEAD~~~~~'
-    alias grev~~~~~~='git revert HEAD~~~~~~'
-    alias grev~~~~~~~='git revert HEAD~~~~~~~'
-    alias grev~~~~~~~~='git revert HEAD~~~~~~~~'
-    alias grev~~~~~~~~~='git revert HEAD~~~~~~~~~'
-    alias grev~~~~~~~~~~='git revert HEAD~~~~~~~~~~'
-    alias grm='git rm'
-    alias grmc='git rm --cached'
-    alias gs='git status'
-    alias gss='git stash'
-    alias gsssm='git stash push -m'
-    alias gssp='git stash pop'
-    function gsssp () {
-        git stash push --message "$1" --patch
-    }
-    alias gssa='git stash apply'
-    alias gssd='git stash drop'
-    alias gssl='git stash list'
-    alias gmt='git mergetool'
-    alias gmerge='git merge --no-ff'
-    alias gsm='git submodule'
-    alias gsma='git submodule add'
-    alias gsmd='git submodule deinit'
-    alias gsmu='git submodule update'
-    alias gsmui='git submodule update --init'
-    alias gsmuir='git submodule update --init --recursive'
-    alias gch='git cherry-pick'
-    alias _gclean='git clean -fd'
-    alias gp='git push'
-    alias gpu='git push -u'
-    alias gpuo='git push -u origin'
-    alias gpua='git push -u aiya000'
-    alias gpf='git push --force-with-lease'
-    alias gpull='git pull --rebase'
-    alias greflog='git reflog'
-    alias gshow='git show'
-    alias gclone='git clone --recurse-submodules'
-    alias gtag-list='git tag | xargs echo'
-    alias gtag-add='git tag -a'
-    alias gf='git fetch'
-    alias gfo='git fetch origin'
-    alias gbi='git bisect'
-    alias gtag='git tag | xargs echo'
-    alias gtree='git log --graph --decorate --oneline'
-    alias gtree-all='git log --graph --decorate --oneline --all'
-    alias gw='git worktree'
-    alias gwa='git worktree add'
+  git_taking_limit=100
 
-    function git-branch-name () {
-        git branch 2> /dev/null | grep '\*\s.*' | awk '{print $2}'
-    }
+  alias _gr='git reset'
+  alias _grh='git reset --hard'
+  alias _grh~='git reset --hard HEAD~'
+  alias _grh~~='git reset --hard HEAD~~'
+  alias _grh~~~='git reset --hard HEAD~~~'
+  alias _grh~~~~='git reset --hard HEAD~~~~'
+  alias _grs='git reset --soft'
+  alias _grs~='git reset --soft HEAD~'
+  alias _grs~~='git reset --soft HEAD~~'
+  alias _grs~~~='git reset --soft HEAD~~~'
+  alias _grs~~~~='git reset --soft HEAD~~~~'
+  alias ga='git add'
+  alias gaa='git add -A'
+  alias gap='git add -p'
+  alias gb='git branch'
+  alias gbd='git branch --delete'
+  alias _gbd='git branch -D'
+  alias _gbdf='git branch --delete --force'
+  alias gbm='git branch -m'
+  alias gc='git commit --verbose'
+  alias gcam='git commit --verbose --amend'
+  alias gcm='git commit -m'
+  alias gcf='git commit --fixup'
+  alias gaacm='git add -A && git commit -m'
+  alias gaacamm='git add -A && git commit --amend -m'
+  alias gco='git checkout'
+  alias gsw='git switch'
+  alias gswc='git switch --create'
+  alias gswd='git switch --detach'
+  alias gres='git restore'
+  alias gd='git diff'
+  alias gdh='git diff HEAD~..HEAD'
+  alias gds='git diff --staged'
+  alias gdst0='git diff stash@{0}'
+  alias gdst1='git diff stash@{1}'
+  alias gdst2='git diff stash@{2}'
+  # shellcheck disable=SC2139
+  alias gl="git log --name-only -$git_taking_limit"
+  # shellcheck disable=SC2139
+  alias glo="git log --oneline -$git_taking_limit"
+  alias gll='glo | head'
+  # shellcheck disable=SC2139
+  alias glp="git log --patch -$git_taking_limit"
+  # shellcheck disable=SC2139
+  alias glf="git log --name-only -$git_taking_limit"
+  alias gmv='git mv'
+  alias gr='git rebase'
+  alias gra='git rebase --abort'
+  alias grc='git rebase --continue'
+  alias gri='git rebase --interactive --autosquash'
+  alias gri~='git rebase --interactive --autosquash HEAD~'
+  alias gri~~='git rebase --interactive --autosquash HEAD~~'
+  alias gri~~~='git rebase --interactive --autosquash HEAD~~~'
+  alias gri~~~~='git rebase --interactive --autosquash HEAD~~~~'
+  alias gri~~~~~='git rebase --interactive --autosquash HEAD~~~~~'
+  alias gri~~~~~~='git rebase --interactive --autosquash HEAD~~~~~~'
+  alias gri~~~~~~~='git rebase --interactive --autosquash HEAD~~~~~~~'
+  alias gri~~~~~~~~='git rebase --interactive --autosquash HEAD~~~~~~~~'
+  alias gri~~~~~~~~~='git rebase --interactive --autosquash HEAD~~~~~~~~~'
+  alias gri~~~~~~~~~~='git rebase --interactive --autosquash HEAD~~~~~~~~~~'
+  alias gr-onto='git rebase --onto'
+  alias grev='git revert'
+  alias grev~='git revert HEAD~'
+  alias grev~~='git revert HEAD~~'
+  alias grev~~~='git revert HEAD~~~'
+  alias grev~~~~='git revert HEAD~~~~'
+  alias grev~~~~~='git revert HEAD~~~~~'
+  alias grev~~~~~~='git revert HEAD~~~~~~'
+  alias grev~~~~~~~='git revert HEAD~~~~~~~'
+  alias grev~~~~~~~~='git revert HEAD~~~~~~~~'
+  alias grev~~~~~~~~~='git revert HEAD~~~~~~~~~'
+  alias grev~~~~~~~~~~='git revert HEAD~~~~~~~~~~'
+  alias grm='git rm'
+  alias grmc='git rm --cached'
+  alias gs='git status'
+  alias gss='git stash'
+  alias gsssm='git stash push -m'
+  alias gssp='git stash pop'
+  function gsssp () {
+      git stash push --message "$1" --patch
+  }
+  alias gssa='git stash apply'
+  alias gssd='git stash drop'
+  alias gssl='git stash list'
+  alias gmt='git mergetool'
+  alias gmerge='git merge --no-ff'
+  alias gsm='git submodule'
+  alias gsma='git submodule add'
+  alias gsmd='git submodule deinit'
+  alias gsmu='git submodule update'
+  alias gsmui='git submodule update --init'
+  alias gsmuir='git submodule update --init --recursive'
+  alias gch='git cherry-pick'
+  alias _gclean='git clean -fd'
+  alias gp='git push'
+  alias gpu='git push -u'
+  alias gpuo='git push -u origin'
+  alias gpua='git push -u aiya000'
+  alias gpf='git push --force-with-lease'
+  alias gpull='git pull --rebase'
+  alias greflog='git reflog'
+  alias gshow='git show'
+  alias gclone='git clone --recurse-submodules'
+  alias gtag-list='git tag | xargs echo'
+  alias gtag-add='git tag -a'
+  alias gf='git fetch'
+  alias gfo='git fetch origin'
+  alias gbi='git bisect'
+  alias gtag='git tag | xargs echo'
+  alias gtree='git log --graph --decorate --oneline'
+  alias gtree-all='git log --graph --decorate --oneline --all'
+  alias gw='git worktree'
+  alias gwa='git worktree add'
 
-    function git-log-unpushed () {
-        git log "$@" "origin/$(git-branch-name)..HEAD"
-    }
+  function _grs_n () {
+    git reset --soft "HEAD~${1}"
+  }
 
-    function gwb () {
-        local branch_name=$1
-        git worktree add "$branch_name" -b "$branch_name"
-    }
+  function gdst_n () {
+    git diff "stash@{${1}}"
+  }
 
-    alias gwl='git worktree list'
-    alias gwp='git worktree prune'
-    alias gw-erase-removed='git worktree prune'
-    unset git_taking_limit
+  function gri_n () {
+    git rebase --interactive --autosquash "HEAD~${1}"
+  }
 
-    function git-push-wip () {
-        local wip_name=wip-current-unique-unique-yazawa-nico
-        git branch -D "$wip_name"
-        git checkout -b "$wip_name"
-        git add -A
-        git commit -m "Today's WIP"
-        git push -f origin "$wip_name"
-        git checkout -
-        git cherry-pick "$wip_name"
-    }
-    alias gp-wip=git-push-wip
-    alias gpw=gp-wip
+  function grev_n () {
+    git revert "HEAD~${1}"
+  }
 
-    function _gre () {
-        echo -n "(at '$(git rev-parse --show-toplevel)') Do you really force reset the git index tree? (y/n)"
-        if read -rq _  ; then
-            echo
-            git reset --hard HEAD
-            echo 'done'
-        fi
-    }
+  function git-branch-name () {
+    git branch 2> /dev/null | grep '\*\s.*' | awk '{print $2}'
+  }
 
-    alias ginit='git init'
+  function git-log-unpushed () {
+    git log "$@" "origin/$(git-branch-name)..HEAD"
+  }
 
-    function github-change-remote-from-git-to-https () {
-        local remote https_url
-        remote=${1:-origin}
-        https_url=$(git remote get-url "$remote" \
-            | sed -r 's/git@([^:]+):([^\/]+)\/(.*)/https:\/\/\1\/\2\/\3/' \
-            | sed -r 's/\.git$//')
-        git remote set-url "$remote" "$https_url"
-        git remote get-url "$remote"
-    }
+  function gwb () {
+    local branch_name=$1
+    git worktree add "$branch_name" -b "$branch_name"
+  }
 
-    # Set casual user.name and user.email at local
-    alias git-set-casual-name='git config --local user.name aiya000 && git config --local user.email aiya000.develop@gmail.com ; git config --local user.name ; git config --local user.email'
-    alias cd-git-root='cd "$(git rev-parse --show-toplevel)"'
-    alias cdg=cd-git-root
+  alias gwl='git worktree list'
+  alias gwp='git worktree prune'
+  alias gw-erase-removed='git worktree prune'
+
+  unset git_taking_limit
+
+  function git-push-wip () {
+    local wip_name=wip-current-unique-unique-yazawa-nico
+    git branch -D "$wip_name"
+    git checkout -b "$wip_name"
+    git add -A
+    git commit -m "Today's WIP"
+    git push -f origin "$wip_name"
+    git checkout -
+    git cherry-pick "$wip_name"
+  }
+
+  function _gre () {
+    echo -n "(at '$(git rev-parse --show-toplevel)') Do you really force reset the git index tree? (y/n)"
+    if read -rq _  ; then
+      echo
+      git reset --hard HEAD
+      echo 'done'
+    fi
+  }
+
+  alias ginit='git init'
+
+  function github-change-remote-from-git-to-https () {
+    local remote https_url
+    remote=${1:-origin}
+    https_url=$(git remote get-url "$remote" \
+      | sed -r 's/git@([^:]+):([^\/]+)\/(.*)/https:\/\/\1\/\2\/\3/' \
+      | sed -r 's/\.git$//')
+    git remote set-url "$remote" "$https_url"
+    git remote get-url "$remote"
+  }
+
+  # Set casual user.name and user.email at local
+  alias git-set-casual-name='git config --local user.name aiya000 && git config --local user.email aiya000.develop@gmail.com ; git config --local user.name ; git config --local user.email'
+  alias cd-git-root='cd "$(git rev-parse --show-toplevel)"'
+  alias cdg=cd-git-root
 fi
 
 # }}}
 # Docker {{{
 
 if i_have docker ; then
-    alias d=docker
-    alias da=docker-attach-menu.sh
-    alias dki=docker-kill-menu.sh
-    alias dkill='docker kill'
-    alias dps='docker ps'
-    alias drm='docker rm'
-    alias drmi='docker rmi'
-    alias drun='docker run'
+  alias d=docker
+  alias da=docker-attach-menu.sh
+  alias dki=docker-kill-menu.sh
+  alias dkill='docker kill'
+  alias dps='docker ps'
+  alias drm='docker rm'
+  alias drmi='docker rmi'
+  alias drun='docker run'
 
-    alias docker-force-remove-all-containers='docker rm -f $(docker ps -a -q)'
-    alias docker-force-remove-all-images='docker rmi -f $(docker images -q)'
-    # shellcheck disable=SC2142
-    alias docker-force-clean-volumes='docker volume rm $(docker volume ls | awk "{print $2}")'
+  alias docker-force-remove-all-containers='docker rm -f $(docker ps -a -q)'
+  alias docker-force-remove-all-images='docker rmi -f $(docker images -q)'
+  # shellcheck disable=SC2142
+  alias docker-force-clean-volumes='docker volume rm $(docker volume ls | awk "{print $2}")'
 
-    function docker-force-remove-all-all () {
-        docker-force-remove-all-containers
-        docker-force-remove-all-images
-        docker-force-clean-volumes
-    }
+  function docker-force-remove-all-all () {
+    docker-force-remove-all-containers
+    docker-force-remove-all-images
+    docker-force-clean-volumes
+  }
 fi
 
 # }}}
@@ -275,12 +291,12 @@ alias ki=killing-art
 # alias ....='cd ../../../'
 # ...
 function aliases::define_cd_to_parents () {
-    local name
-    for (( i = 2; i <= 20; ++i )) ; do
-        name=$(eval "printf '.%.0s' {1..$i}")
-        dir=$(eval "printf '../%.0s' {2..$i}")
-        eval "alias $name='cd $dir'"
-    done
+  local name
+  for (( i = 2; i <= 20; ++i )) ; do
+    name=$(eval "printf '.%.0s' {1..$i}")
+    dir=$(eval "printf '../%.0s' {2..$i}")
+    eval "alias $name='cd $dir'"
+  done
 }
 aliases::define_cd_to_parents
 
@@ -297,80 +313,80 @@ i_have unzip && alias unzip-cp932='unzip -O cp932'
 i_have xdg-open && alias x=xdg-open
 
 if i_have ctags ; then
-    function ctags-auto () {
-        local root
-        root=$(git rev-parse --show-toplevel 2> /dev/null || pwd)
-        dest=$([[ -d $root/.git ]] && echo "$root/.git/tags" || echo "$root/tags")
-        ctags -f "$dest" --tag-relative=never --recurse --sort=yes "$@"
-    }
+  function ctags-auto () {
+    local root
+    root=$(git rev-parse --show-toplevel 2> /dev/null || pwd)
+    dest=$([[ -d $root/.git ]] && echo "$root/.git/tags" || echo "$root/tags")
+    ctags -f "$dest" --tag-relative=never --recurse --sort=yes "$@"
+  }
 
-    alias ctags-kotlin-auto="ctags-auto '--exclude=*.java' '--exclude=*.html' '--exclude=*.css'"
-    alias ctags-typescript-auto="ctags-auto '--exclude=*.js' '--exclude=*.json'"
+  alias ctags-kotlin-auto="ctags-auto '--exclude=*.java' '--exclude=*.html' '--exclude=*.css'"
+  alias ctags-typescript-auto="ctags-auto '--exclude=*.js' '--exclude=*.json'"
 fi
 
 if i_have yay ; then
-    alias yay='yay --color always'
+  alias yay='yay --color always'
 fi
 
 if i_have stack ; then
-    alias si='stack install'
-    alias srunghc='stack runghc --'
-    alias sghci='stack ghci --'
-    alias stack-haddock-gen='stack haddock .'
-    alias shaddock-gen='stack haddock .'
-    alias shaddock-gen-open='stack haddock --open .'
-    function stack-new-default() {
-        stack new "$1" simple
-    }
-    alias stack-build-profile='stack build --profile'
-    alias make-new-package-yaml='cp ~/.dotfiles/Files/package.yaml .'
+  alias si='stack install'
+  alias srunghc='stack runghc --'
+  alias sghci='stack ghci --'
+  alias stack-haddock-gen='stack haddock .'
+  alias shaddock-gen='stack haddock .'
+  alias shaddock-gen-open='stack haddock --open .'
+  function stack-new-default() {
+    stack new "$1" simple
+  }
+  alias stack-build-profile='stack build --profile'
+  alias make-new-package-yaml='cp ~/.dotfiles/Files/package.yaml .'
 
-    alias cabal-sdit='stack exec -- cabal sdist'
-    function cabal-upload() {
-        stack exec -- cabal upload "$1"
-    }
+  alias cabal-sdit='stack exec -- cabal sdist'
+  function cabal-upload() {
+    stack exec -- cabal upload "$1"
+  }
 fi
 
 if i_have cabal ; then
-    alias ci='cabal new-install'
+  alias ci='cabal new-install'
 fi
 
 if i_have idris ; then
-    function run-idris() {
-        if idris "$1" -o "/tmp/$1.idris-run-output" && "/tmp/$1.idris-run-output" ; then
-            # shellcheck disable=SC2028
-            # shellcheck disable=SC1117
-            echo "\n>>> run-idris is succeed"
-        else
-            # shellcheck disable=SC2028
-            # shellcheck disable=SC1117
-            echo "\n>>> run-idris is failed"
-        fi
-        if [[ -f $1 ]] ; then
-            rm "/tmp/$1.idris-run-output"
-        fi
-    }
-    alias runidris=run-idris
+  function run-idris() {
+    if idris "$1" -o "/tmp/$1.idris-run-output" && "/tmp/$1.idris-run-output" ; then
+      # shellcheck disable=SC2028
+      # shellcheck disable=SC1117
+      echo "\n>>> run-idris is succeed"
+    else
+      # shellcheck disable=SC2028
+      # shellcheck disable=SC1117
+      echo "\n>>> run-idris is failed"
+    fi
+    if [[ -f $1 ]] ; then
+      rm "/tmp/$1.idris-run-output"
+    fi
+  }
+alias runidris=run-idris
 fi
 
 if [[ -e ./gradlew ]] ; then
-    alias grwj='./gradlew jar'
+  alias grwj='./gradlew jar'
 fi
 
 if i_have krita ; then
-    alias kra=krita
+  alias kra=krita
 fi
 
 if i_have draw.io ; then
-    alias drio=draw.io
+  alias drio=draw.io
 
-    # NOTE: Before, draw.io exports foo.xml.png for foo.xml, but the backward compatility broke.
-    function drio-export-png () {
-        local xml_name
-        xml_name=$1
+  # NOTE: Before, draw.io exports foo.xml.png for foo.xml, but the backward compatility broke.
+  function drio-export-png () {
+    local xml_name
+    xml_name=$1
 
-        draw.io --embed-diagram --export --format png "$xml_name" --output "${xml_name}.png"
-    }
+    draw.io --embed-diagram --export --format png "$xml_name" --output "${xml_name}.png"
+  }
 fi
 
 # shellcheck disable=SC2139
@@ -381,17 +397,17 @@ alias mount4u.ext3=mount4u.ext2
 alias mount4u.ext4=mount4u.ext2
 
 function virtualenv-activate () {
-    if [[ -f ./.venv/bin/activate ]] ; then
-        # shellcheck disable=SC1091
-        . ./.venv/bin/activate
-    else
-        echo './.venv/bin/activate was not found, please load it yourself...' > /dev/stderr
-        return 1
-    fi
+  if [[ -f ./.venv/bin/activate ]] ; then
+    # shellcheck disable=SC1091
+    . ./.venv/bin/activate
+  else
+    echo './.venv/bin/activate was not found, please load it yourself...' > /dev/stderr
+    return 1
+  fi
 }
 
 function grepall () {
-    find . -type f -print0 | xargs -0 -I {} bash -c "grep $* {} && (echo '{}' ; echo '- - - - - - - - - -')"
+  find . -type f -print0 | xargs -0 -I {} bash -c "grep $* {} && (echo '{}' ; echo '- - - - - - - - - -')"
 }
 
 # }}}
