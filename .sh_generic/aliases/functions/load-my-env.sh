9cf053efefd1aea4fa1ddc61ba5c0b812972645b
aiya000 <aiya000.develop@gmail.com>
2021-05-04 18:12:59 +0900

    improve it

--- .sh_generic/aliases/functions/load-my-env.sh
+++ .sh_generic/aliases/functions/load-my-env.sh
@@ -2,71 +2,64 @@
 
 function load-my-env () {
   local target_name="$1"
+
   case "$target_name" in
     all)
       load-my-env stack
       load-my-env cabal
       load-my-env pkgsrc
-      load-my-env ruby
-      load-my-env zsh
-      load-my-env bash
+      load-my-env rbenv
       load-my-env travis
       load-my-env ccache
       load-my-env linuxbrew
       ;;
+
     stack)
-      if [ -d ~/.stack ] ; then
+      if [[ -d ~/.stack ]] ; then
         PATH=$PATH:$HOME/.stack/programs/x86_64-linux/ghc-7.8.4/bin
       fi
-      #FIXME: Avoid to load that is failure on the cygwin
-      if [[ $(uname | grep -i cygwin) == '' ]] ; then
-        # Use stack-completion
-        if type stack > /dev/null 2>&1 ; then
-          # This completion needs compinit and bashcompinit function
-          # > autoload -U compinit     && compinit
-          # > autoload -U bashcompinit && bashcompinit
-          eval "$(stack --bash-completion-script stack)"
-        fi
-      fi
+      echo "$HOME/.stack is not found." > /dev/stderr
       ;;
+
     cabal)
-      if [ -d ~/.cabal ] ; then
+      if [[ -d ~/.cabal ]] ; then
         PATH=$PATH:$HOME/.cabal/bin:./.cabal-sandbox/bin
       fi
+      echo "$HOME/.cabal is not found." > /dev/stderr
       ;;
+
     pkgsrc)
-      if [ -d ~/pkg ] ; then
+      if [[ -d ~/pkg ]] ; then
         PATH=$PATH:$HOME/pkg/bin:$HOME/pkg/sbin
+        return
       fi
+      echo "$HOME/pkg is not found." > /dev/stderr
       ;;
-    ruby)
-      if [ -d ~/.rbenv ] ; then
-        PATH=$PATH:$HOME/.rbenv/bin
-        PATH=$PATH:$HOME/.rbenv/versions/$(cat ~/.rbenv/version)/bin
+
+    rbenv)
+      if [[ -d ~/.rbenv ]] ; then
+        PATH=$HOME/.rbenv/bin:$HOME/.rbenv/versions/$(cat ~/.rbenv/version)/bin:$PATH
         eval "$("$HOME/.rbenv/bin/rbenv" init -)"
+      else
+        echo "$HOME/.rbenv is not found." > /dev/stderr
       fi
-      if [ -d ~/.rbenv/plugins/ruby-build/bin ] ; then
+
+      if [[ -d ~/.rbenv/plugins/ruby-build/bin ]] ; then
         PATH=$PATH:$HOME/.rbenv/plugins/ruby-build/bin
+      else
+        echo "$HOME/.rbenv/plugins/ruby-build/bin is not found." > /dev/stderr
       fi
       ;;
-    zsh)
-      export ZAPACK_OPTIONS='--verbose'
-      if [ -f /usr/share/git/completion/git-completion.zsh ] && [ ! -f "$ZDOTDIR/_git" ] ; then
-        cp /usr/share/git/completion/git-completion.zsh "$ZDOTDIR/_git"
-      fi
-      ;;
-    bash)
-      if [ -f "$HOME/.bashfiles/git-completion.bash" ] ; then
-        # shellcheck disable=SC1090
-        source "$HOME/.bashfiles/git-completion.bash"
-      fi
-      ;;
+
     travis)
-      if [ -f ~/.travis/travis.sh ] ; then
+      if [[ -f ~/.travis/travis.sh ]] ; then
         # shellcheck disable=SC1090
         source ~/.travis/travis.sh
+        return
       fi
+      echo "$HOME/.travis/travis.sh is not found." > /dev/stderr
       ;;
+
     ccache)
       export USE_CCACHE=1
       export CCACHE_DISABLE=0
@@ -86,9 +79,11 @@ function load-my-env () {
         echo export CCACHE_DIR=~/.ccache
       fi
       ;;
+
     linuxbrew)
       eval "$(~/.linuxbrew/bin/brew shellenv)"
       ;;
+
     *)
       echo "You may haven't $target_name" > /dev/stderr
       ;;
