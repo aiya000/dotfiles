89a1a6171fa67152f284bf34191ea499c0efcef7
aiya000 <aiya000.develop@gmail.com>
2021-08-12 21:18:52 +0900

    :+1: cdg

--- .sh_generic/aliases/functions/cd-to-git-root.sh
+++ .sh_generic/aliases/functions/cd-to-git-root.sh
@@ -2,13 +2,14 @@
 
 function cd-to-git-root () {
   local root simple
-  root=$(git rev-parse --show-toplevel 2> /dev/null || exit 1)
+  root=$(git rev-parse --show-toplevel 2> /dev/null || return 1)
+  cd "$root" 2>&1  # Don't use a variable to avoid executing in a sub shell
 
   # Try recover with wslpath if simple is failed
-  if ! simple=$(cd "$root" 2>&1) && command -v wslpath > /dev/null 2>&1 ; then
+  if [[ $? -ne 0 ]] && command -v wslpath > /dev/null 2>&1 ; then
     root=$(wslpath "$root")
     echo "cd-to-git-root: Using wslpath: $root"
-    cd "$root" || exit 1
+    cd "$root" || return 1
     return
   fi
 
