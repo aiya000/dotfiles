14ef0c1e150f0f68e5ee56d2b6af43ab3134449e
aiya000 <aiya000.develop@gmail.com>
2023-08-24 15:03:28 +0900

    :sparkles: start.sh

--- Windows/WSL2/root/home/foo/start.sh
+++ Windows/WSL2/root/home/foo/start.sh
@@ -6,5 +6,13 @@ rm /tmp/zsh-has-loaded 2> /dev/null || true
 # shellcheck disable=SC1090
 source "$HOME/.dotfiles/root/etc/profile.d/zdotroot.sh"
 
+if [[ ! -d /mnt/g/マイドライブ ]] ; then
+  sudo mount -t drvfs G: /mnt/g
+fi
+
+if [[ $(service cron status) = ' * cron is not running' ]] ; then
+  sudo service cron start
+fi
+
 cd || exit 1
 exec tmux
