84c71cfe44845ce6c3558d62d228529c5176b581
aiya000 <aiya000.develop@gmail.com>
2025-01-17 01:09:11 +0900

    WSL2 Examples

--- Windows/WSL2/root/home/foo/start.sh
+++ Windows/WSL2/root/home/foo/start.sh
@@ -6,8 +6,11 @@ rm /tmp/zsh-has-loaded 2> /dev/null || true
 # shellcheck disable=SC1090
 source "$HOME/.dotfiles/root/etc/profile.d/zdotroot.sh"
 
-if [[ ! -d /mnt/g/マイドライブ ]] ; then
-  sudo mount -t drvfs G: /mnt/g
+# if [[ ! -d /mnt/g/マイドライブ ]] ; then
+#   sudo mount -t drvfs G: /mnt/g
+# fi
+if [[ ! -d '/mnt/p/pCloud Backup' ]] ; then
+  sudo mount -t drvfs -o noatime,uid=1000,gid=1000 P: /mnt/p
 fi
 
 if [[ $(service cron status) = ' * cron is not running' ]] ; then
