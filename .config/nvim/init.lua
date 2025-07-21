69ac536002283538d3d803589b1dcd4c7e1b3cb2
aiya000 <aiya000.develop@gmail.com>
2025-07-21 22:50:18 +0900

    update(Neovim > init.lua): Remove debugging comments & Format by stylua & Add a part of section comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,22 @@
 -- NOTE: Don't use the mark Z, this is reserved by some my functions.
 
--- Debug: Confirm init.lua is being loaded (remove these lines when satisfied)
--- print("🚀 init.lua is loading...")
--- vim.cmd('echo "✅ init.lua loaded successfully!"')
+-------------------
+-- Global values --
+-------------------
+-- vim.g.vimrc {{{
 
--- Global values
-vim.g.vimrc = vim.g.vimrc or {
-  loaded = false,
-  vim_home = vim.fn.expand('~/.config/nvim'),
-  path_at_started = vim.fn.getcwd(),
-  is_wsl = vim.fn.executable('uname') == 1 and vim.fn.system('uname -a'):match('microsoft%-standard'),
-  is_unix = vim.fn.has('unix') == 1,
-  is_macos = vim.fn.has('macunix') == 1,
-  git_root = nil,
-  memo_path = '~/.backup/memo.md',
-  is_ddc_enabled = nil,
-}
+vim.g.vimrc = vim.g.vimrc
+  or {
+    loaded = false,
+    vim_home = vim.fn.expand('~/.config/nvim'),
+    path_at_started = vim.fn.getcwd(),
+    is_wsl = vim.fn.executable('uname') == 1 and vim.fn.system('uname -a'):match('microsoft%-standard'),
+    is_unix = vim.fn.has('unix') == 1,
+    is_macos = vim.fn.has('macunix') == 1,
+    git_root = nil,
+    memo_path = '~/.backup/memo.md',
+    is_ddc_enabled = nil,
+  }
 
 -- Call git root setup function (delayed to avoid startup slowdown)
 vim.defer_fn(function()
@@ -23,10 +24,10 @@ vim.defer_fn(function()
   vimrc.read_git_root_to_set_g_vimrc_async()
 end, 100)
 
-vim.g.vimrc.open_on_gui = vim.g.vimrc.is_macos and 'open' or
-                         vim.g.vimrc.is_wsl and 'wslview' or
-                         vim.g.vimrc.is_unix and 'xdg-open' or
-                         'echo "no method for GUI-open"'
+vim.g.vimrc.open_on_gui = vim.g.vimrc.is_macos and 'open'
+  or vim.g.vimrc.is_wsl and 'wslview'
+  or vim.g.vimrc.is_unix and 'xdg-open'
+  or 'echo "no method for GUI-open"'
 
 local backupdir = vim.fn.expand('$HOME/.backup/vim-backup')
 vim.g.vimrc.backupdir = backupdir
@@ -58,6 +59,9 @@ vim.g.vimrc.temporary_buftypes = {
   'fern',
 }
 
+-- }}}
+-- Others {{{
+
 -- TODO: Remove this after https://github.com/aiya000/bash-toys/issues/12 fixed
 -- Please see https://github.com/aiya000/bash-toys
 vim.env.BASH_TOYS_DUSTBOX_DIR = vim.fn.expand('$HOME/.backup/dustbox')
@@ -70,13 +74,22 @@ local typescript_variants = {
   'javascript.jsx',
 }
 
--- Set encoding
+-- }}}
+
+
+-------------
+-- Prepare --
+-------------
+-- Set encoding {{{
+
 if not vim.g.vimrc.loaded then
   vim.opt.fileencoding = 'utf-8'
   vim.opt.encoding = 'utf-8'
 end
 
--- Prepare backup directories
+-- }}}
+-- Prepare backup directories {{{
+
 local function ensure_directory(dir)
   if vim.fn.isdirectory(dir) == 0 then
     vim.fn.mkdir(dir, 'p', 0x1c0) -- 0700 in octal
@@ -87,6 +100,8 @@ end
 ensure_directory(vim.g.vimrc.directory)
 ensure_directory(vim.g.vimrc.undodir)
 ensure_directory(vim.g.vimrc.sessiondir)
+
+-- }}}
 
 -- Load private configuration
 local private_vimrc = vim.fn.expand('$HOME/.dotfiles/.private/.vimrc_private')
@@ -101,7 +116,7 @@ end
 
 -- Options
 vim.opt.autoindent = true
-vim.opt.backspace = {'indent', 'eol', 'start'}
+vim.opt.backspace = { 'indent', 'eol', 'start' }
 vim.opt.breakindent = true
 -- vim.opt.browsedir = 'buffer'  -- Not supported in Neovim
 vim.opt.cindent = true
@@ -110,7 +125,20 @@ vim.opt.cmdwinheight = 20
 vim.opt.completeopt:remove('preview')
 vim.opt.conceallevel = 1
 vim.opt.expandtab = true
-vim.opt.fileencodings = {'ucs-bom', 'utf-8', 'sjis', 'euc-jp', 'cp932', 'iso-2022-jp-3', 'iso-2022-jp', 'eucjp-ms', 'euc-jisx0213', 'ucs-bom', 'latin1', 'default'}
+vim.opt.fileencodings = {
+  'ucs-bom',
+  'utf-8',
+  'sjis',
+  'euc-jp',
+  'cp932',
+  'iso-2022-jp-3',
+  'iso-2022-jp',
+  'eucjp-ms',
+  'euc-jisx0213',
+  'ucs-bom',
+  'latin1',
+  'default',
+}
 vim.opt.helplang = 'en'
 vim.opt.hidden = true
 vim.opt.history = 1000
@@ -119,8 +147,8 @@ vim.opt.incsearch = true
 vim.opt.laststatus = 2
 vim.opt.linebreak = true
 vim.opt.list = true
-vim.opt.listchars = {tab = '»_', trail = '_', extends = '»', precedes = '«', nbsp = '%', eol = '↲'}
-vim.opt.matchpairs:append({'<:>', '（:）', '｛:｝', '「:」', '＜:＞', '『:』', '【:】'})
+vim.opt.listchars = { tab = '»_', trail = '_', extends = '»', precedes = '«', nbsp = '%', eol = '↲' }
+vim.opt.matchpairs:append({ '<:>', '（:）', '｛:｝', '「:」', '＜:＞', '『:』', '【:】' })
 vim.opt.foldenable = false
 vim.opt.joinspaces = false
 vim.opt.ruler = false
@@ -129,11 +157,11 @@ vim.opt.wrap = false
 vim.opt.wrapscan = false
 vim.opt.number = true
 vim.opt.omnifunc = 'v:lua.vim.lsp.omnifunc'
-vim.opt.path = {'.',',', './*'}
+vim.opt.path = { '.', ',', './*' }
 vim.opt.previewheight = 40
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 16
-vim.opt.sessionoptions = {'buffers', 'tabpages', 'localoptions', 'winsize', 'winpos'}
+vim.opt.sessionoptions = { 'buffers', 'tabpages', 'localoptions', 'winsize', 'winpos' }
 vim.opt.shellslash = true
 vim.opt.shiftwidth = 2
 vim.opt.suffixes = {}
@@ -148,10 +176,10 @@ function _G.simple_tabline()
   local line = ''
   local tab_count = vim.fn.tabpagenr('$')
   local current_tab = vim.fn.tabpagenr()
-  
+
   -- Show tab count
   line = line .. '[' .. tab_count .. '] '
-  
+
   -- Show tabs
   for i = 1, tab_count do
     local buflist = vim.fn.tabpagebuflist(i)
@@ -159,11 +187,11 @@ function _G.simple_tabline()
     local bufnr = buflist[winnr]
     local bufname = vim.fn.bufname(bufnr)
     local filename = vim.fn.fnamemodify(bufname, ':t')
-    
+
     if filename == '' then
       filename = '[NoName]'
     end
-    
+
     -- Highlight current tab
     if i == current_tab then
       line = line .. '[* ' .. filename .. ' *]'
@@ -171,19 +199,16 @@ function _G.simple_tabline()
       line = line .. '[' .. filename .. ']'
     end
   end
-  
+
   -- Show working directory
   local cwd = vim.fn.getcwd()
   line = line .. ' => [PWD=' .. vim.fn.fnamemodify(cwd, ':~') .. ']'
-  
+
   return line
 end
 
 -- Tabline - temporary simple version until we convert vimrc#tabline#make()
 vim.opt.tabline = '%!v:lua.simple_tabline()'
-
--- Debug: Options loaded
--- print("⚙️  Basic options configured")
 
 vim.opt.ambiwidth = 'double'
 vim.opt.background = 'dark'
@@ -194,12 +219,12 @@ vim.cmd('colorscheme default')
 
 vim.opt.showtabline = 2
 
--- Set the fold options  
+-- Set the fold options
 vim.opt.foldcolumn = '1'
-vim.opt.foldopen = {'search', 'jump', 'mark', 'percent', 'insert', 'tag', 'undo'}
+vim.opt.foldopen = { 'search', 'jump', 'mark', 'percent', 'insert', 'tag', 'undo' }
 vim.opt.foldclose = 'all'
 vim.opt.foldmethod = 'marker'
-vim.opt.fillchars = {vert = '|', fold = ' '}
+vim.opt.fillchars = { vert = '|', fold = ' ' }
 
 -- The backup options
 vim.opt.directory = vim.g.vimrc.directory
@@ -243,7 +268,7 @@ end)
 
 if not dein_ok then
   -- If dein.vim is not found, we'll continue without plugins for now
-  print("Note: dein.vim not found. Continuing without plugin manager.")
+  print('Note: dein.vim not found. Continuing without plugin manager.')
 end
 
 -- Load configuration modules
@@ -263,7 +288,3 @@ vim.cmd('filetype plugin indent on')
 vim.cmd('syntax enable')
 
 vim.g.vimrc.loaded = true
-
--- Debug: Confirm init.lua completed loading
--- print("🎉 init.lua loading completed!")
--- vim.cmd('echo "🎯 All configurations loaded successfully!"')
