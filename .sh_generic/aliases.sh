2b12a51d51f5ba137a60cf776ab9bb30e45d1361
aiya000 <aiya000.develop@gmail.com>
2025-08-20 01:48:33 +0900

    feat(Shell): Add wrapper functions for SoundVolumeView.exe

--- .sh_generic/aliases.sh
+++ .sh_generic/aliases.sh
@@ -129,7 +129,58 @@ if i_have notifu.exe && ! i_have notify-send ; then
   }
 fi
 
-## Editor {{{
+if i_have soundvolumeview ; then
+  function soundvolumeview-list () {
+    local tmpfile="/tmp/$RANDOM"
+    soundvolumeview /stext "$tmpfile"
+    cat "$tmpfile" | rg '^Device Name' | sort | uniq
+    rm "$tmpfile" > /dev/null
+  }
+
+  function soundvolumeview-set-default () {
+    : Exected: 'output' or 'input'
+    local device_type=$1
+    local device_name=${2:-$(soundvolumeview-list | peco | awk '{print $3}')}
+
+    : 0   - 'Console'
+    : 1   - 'Multimedia (Speaker)'
+    : 2   - 'Communications (Mic)'
+    : all - 'Set all default types (Console, Multimedia, and Communications)'
+    :
+    : See https://www.nirsoft.net/utils/sound_volume_view.html
+    case $device_type in
+      output) local device_type_number=1 ;;
+      input) local device_type_number=2 ;;
+      *)
+        echo "Unknown device type: $device_type"
+        return 1
+        ;;
+    esac
+
+    soundvolumeview /SetDefault "$device_name" "$device_type_number"
+  }
+
+  function soundvolumeview-get-default () {
+    : Exected: 'output' or 'input'
+    local device_type=$1
+    case $device_type in
+      output) local device_type_name='Render' ;;
+      input) local device_type_name='Capture' ;;
+      *)
+        echo "Unknown device type: $device_type"
+        return 1
+        ;;
+    esac
+
+    local tmpfile="/tmp/$RANDOM"
+    soundvolumeview /sjson "$tmpfile"
+    iconv -f UTF-16LE -t UTF-8 "$tmpfile" | jq ".[] | select(.Default == \"$device_type_name\")" | jq '.["Device Name"]' -r
+    rm "$tmpfile" > /dev/null
+  }
+fi
+
+# }}}
+# Editor {{{
 
 # shellcheck disable=SC2139
 alias e="$EDITOR"
@@ -142,8 +193,6 @@ alias gS='ls -A $VIM_SESSION | peco | xargs -I {} gvim -S $VIM_SESSION/{}'
 alias e-current-session="vim-current-session $EDITOR"
 # shellcheck disable=SC2139
 alias g-current-session="vim-current-session $MY_GUI_EDITOR"
-
-# }}}
 
 # }}}
 # Git {{{
@@ -496,7 +545,7 @@ function kill-vue-lsp-servers () {
 }
 
 # }}}
-# Another Not General Commands {{{
+# Another Contextual Commands {{{
 
 alias ctags-kotlin-auto="ctags-auto '--exclude=*.java' '--exclude=*.html' '--exclude=*.css'"
 alias ctags-typescript-auto="ctags-auto '--exclude=*.js' '--exclude=*.json'"
