e5a1db3c3e545038d8ce7786c83e3759355c1a68
aiya000 <aiya000.develop@gmail.com>
2025-09-04 15:43:37 +0900

    change(Vim & Neovim): Change main editor to Neovim. Thanks Vim a lot `:D`

--- .sh_generic/vars.sh
+++ .sh_generic/vars.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-export EDITOR=vim
-export GIT_EDITOR=vim
-export VISUAL=vim
-export MANPAGER='vim - +MANPAGER -c "setl nonu nornu nolist ft=man | only"' # vim-manpager
+export EDITOR=nvim
+export GIT_EDITOR=nvim
+export VISUAL=nvim
+export MANPAGER='nvim - +MANPAGER -c "setl nonu nornu nolist ft=man | only"' # vim-manpager
 export VIM_SESSION=$HOME/.backup/vim-backup/session
 export GOPATH=~/.GoPath
 export LICE_USER=aiya000
 export CCACHE_DISABLE=1
 export THEMIS_HOME=$HOME/.vim/bundle/repos/github.com/thinca/vim-themis
-export NVIM_PYTHON_LOG_FILE=$HOME/.vim/.log/nvim_python.log
+export NVIM_PYTHON_LOG_FILE=$HOME/.config/nvim/.log/nvim_python.log
 export PYENV_ROOT=$HOME/.pyenv
 export NVM_DIR=$HOME/.nvm
 export DENO_INSTALL=$HOME/.deno
