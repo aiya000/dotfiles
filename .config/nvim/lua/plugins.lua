b21734dba0d47a0ecbebc8c9ba9f4392bf7fe54e
aiya000 <aiya000.develop@gmail.com>
2025-09-10 00:26:21 +0900

    feat(Neovim): Install nvim-notify
    Great!!!!!!!!!!!

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -92,6 +92,31 @@ return {
 
   -- UI {{{
   {
+    'rcarriga/nvim-notify',
+    config = function()
+      local notify = require('notify')
+      notify.setup({
+        background_colour = "#000000",
+        fps = 30,
+        icons = {
+          DEBUG = "",
+          ERROR = "",
+          INFO = "",
+          TRACE = "✎",
+          WARN = ""
+        },
+        level = 2,
+        minimum_width = 50,
+        render = "default",
+        stages = "fade_in_slide_out",
+        timeout = 3000,
+        top_down = true
+      })
+      vim.notify = notify
+    end,
+  },
+
+  {
     'NTBBloodbath/galaxyline.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
