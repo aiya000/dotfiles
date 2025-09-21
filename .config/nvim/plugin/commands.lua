8bf00c2c82c0dbe58e39cee5cc0cd889d502f737
aiya000 <aiya000.develop@gmail.com>
2025-09-21 10:05:14 +0900

    feat(Neovim): Refactor commands.lua & Fix `:CdNodeRoot` command and simular commands working

--- .config/nvim/plugin/commands.lua
+++ .config/nvim/plugin/commands.lua
@@ -3,16 +3,20 @@ local git = require('git')
 local nodejs = require('nodejs')
 local s = require('utils.functions').s
 
----@class CreateCommandOptions
----@field nargs? 0 | 1 | '*' | '?' | '+'
----@field bar? boolean
----@field complete? string | function --補完に表示するリスト種類か、リストを返す関数
+---@param cmd_name string
+---@param func string | function --実行されるVimコマンド、もしくは処理
+---@param options? vim.api.keyset.user_command
+local function create_command(cmd_name, func, options)
+  -- with default options
+  options = vim.tbl_extend('keep', options or {}, {
+    bar = true,
+  })
+  vim.api.nvim_create_user_command(cmd_name, func, options)
+end
 
----@param name string --コマンド名
----@param command string | function --実行されるVimコマンド、もしくは処理
----@param options? CreateCommandOptions
-local function create_command(name, command, options)
-  vim.api.nvim_create_user_command(name, command, options)
+local function read_node_base_dir()
+  local ok, result = pcall(helper.get_current_buffer_dir, InitLua.git_root)
+  return ok and result or InitLua.path_at_started or error('No base directory found')
 end
 
 -- Neovim echo systems {{{
@@ -22,12 +26,12 @@ create_command('FtpluginEditAfter', function(opts)
   local path =
     s('{neovim_home}/after/ftplugin/{filetype}.lua', { neovim_home = InitLua.neovim_home, filetype = filetype })
   vim.cmd(s('edit {path}', { path = path }))
-end, { nargs = '?', complete = 'filetype', bar = true })
+end, { nargs = '?', complete = 'filetype' })
 
 create_command('FtDetectEdit', function(opts)
   local filetype = opts.args ~= '' and opts.args or vim.bo.filetype
   vim.cmd(s('edit {neovim_home}/ftdetect/{filetype}.vim', { neovim_home = InitLua.neovim_home, filetype = filetype }))
-end, { nargs = '?', complete = 'filetype', bar = true })
+end, { nargs = '?', complete = 'filetype' })
 
 -- }}}
 -- Git Utils {{{
@@ -40,22 +44,22 @@ create_command('GitCommitFixup', function(opts)
   else
     vim.notify('Failed to create a fixup commit: ' .. (result.stderr or 'Unknown error'), vim.log.levels.ERROR)
   end
-end, { nargs = 1, bar = true })
+end, { nargs = 1 })
 
 create_command('GitTree', function(opts)
   vim.cmd('GinLog --graph --decorate --oneline ' .. vim.fn.string(opts.args))
-end, { nargs = '*', bar = true })
+end, { nargs = '*' })
 
 create_command('GitTreeAll', function(opts)
   vim.cmd('GinLog --graph --decorate --oneline --all' .. opts.args)
-end, { nargs = '*', bar = true })
+end, { nargs = '*' })
 
 -- }}}
 -- vim-webpage {{{
 
 create_command('Weblio', function(opts)
   vim.cmd(s('WebpageShow weblio {args}', { args = opts.args }))
-end, { nargs = '+', bar = true })
+end, { nargs = '+' })
 
 create_command('Stackage', function(opts)
   vim.cmd(s('WebpageShow stackage {args}', { args = opts.args }))
@@ -68,99 +72,100 @@ end, { nargs = '+' })
 create_command('CdBufDir', function()
   local buf_dir = vim.fn.fnameescape(vim.fn.expand('%:p:h'))
   vim.cmd(s('cd {buf_dir}', { buf_dir = buf_dir }))
-end, { bar = true })
+end)
 
 create_command('CdStarted', function()
   vim.cmd(s('cd {path_at_started}', { path_at_started = InitLua.path_at_started }))
-end, { bar = true })
+end)
 
 create_command('CdGitRoot', function()
   git.cd_git_root('cd')
-end, { bar = true })
+end)
 
 create_command('CdNodeRoot', function()
-  nodejs.cd_node_root('cd')
-end, { bar = true })
+  nodejs.cd_node_root('cd', read_node_base_dir())
+end)
 
 -- :lcd
 create_command('LcdBufDir', function()
   local buf_dir = vim.fn.fnameescape(vim.fn.expand('%:p:h'))
   vim.cmd(s('lcd {buf_dir}', { buf_dir = buf_dir }))
-end, { bar = true })
+end)
 
 create_command('LcdStarted', function()
   vim.cmd(s('lcd {path_at_started}', { path_at_started = InitLua.path_at_started }))
-end, { bar = true })
+end)
 
 create_command('LcdGitRoot', function()
   git.cd_git_root('lcd')
-end, { bar = true })
+end)
 
 create_command('LcdNodeRoot', function()
-  nodejs.cd_node_root('lcd')
-end, { bar = true })
+  nodejs.cd_node_root('lcd', read_node_base_dir())
+end)
 
 -- :tcd
 create_command('TcdBufDir', function()
   local buf_dir = vim.fn.fnameescape(vim.fn.expand('%:p:h'))
   vim.cmd(s('tcd {buf_dir}', { buf_dir = buf_dir }))
-end, { bar = true })
+end)
 
 create_command('TcdStarted', function()
   vim.cmd(s('tcd {path_at_started}', { path_at_started = InitLua.path_at_started }))
-end, { bar = true })
+end)
 
 create_command('TcdGitRoot', function()
   git.cd_git_root('tcd')
-end, { bar = true })
+end)
 
 create_command('TcdNodeRoot', function()
-  nodejs.cd_node_root('tcd')
-end, { bar = true })
+  nodejs.cd_node_root('tcd', read_node_base_dir())
+end)
 
 -- g:vimrc.path_at_started assignment
 create_command('ScdBufDir', function()
   InitLua.path_at_started = vim.fn.expand('%:p:h')
-end, { bar = true })
+end)
 
 create_command('ScdCurrentDir', function()
   InitLua.path_at_started = vim.fn.getcwd()
-end, { bar = true })
+end)
 
 create_command('ScdGitRoot', function()
   InitLua.path_at_started = InitLua.git_root
-end, { bar = true })
+end)
 
 create_command('ScdNodeRoot', function()
   -- TODO: Implement equivalent for s:Msg.error
   vim.cmd('echohl ErrorMsg | echo "Not implemented yet" | echohl None')
-end, { bar = true })
+end)
 
 -- }}}
 
 -- Clear quickfix
 create_command('CClear', function()
   vim.fn.setqflist({})
-end, { bar = true })
+end)
 
 -- Rename a file of the current buffer
 create_command('Rename', function(opts)
   helper.rename_to(opts.args)
-end, { nargs = 1, complete = 'file', bar = true })
+end, { nargs = 1, complete = 'file' })
 
+-- TODO: Not working well
 -- Rename variables by lsp
-create_command('LspRename', vim.lsp.buf.references, { bar = true })
+create_command('LspRename', vim.lsp.buf.references)
 
 create_command('LspFormat', function()
   vim.lsp.buf.format({ async = true })
-end, { bar = true })
+end)
 
 -- TODO: 普通にautofixプラグインを使う（aleあたり）
 create_command('KtlintAutoFix', function()
   local current_file = vim.fn.fnameescape(vim.fn.expand('%'))
   vim.fn.system(s('ktlint --format {current_file}', { current_file = current_file }))
   vim.cmd('edit %')
-end, { bar = true })
+end)
 
 ---@param search_word string
 create_command('Grep', function(search_word)
@@ -180,30 +185,30 @@ create_command('Grep', function(search_word)
       },
     },
   }, search_word)
-end, { nargs = '?', bar = true })
+end, { nargs = '?' })
 
-create_command('ReverseLines', '!tac', { bar = true })
+create_command('ReverseLines', '!tac')
 
 create_command('ReplaceListSign', function()
   vim.cmd("'<,'>s/\\(\\s*\\)- /\\1・ /")
-end, { range = true, bar = true })
+end, { range = true })
 
 -- deepl.vim
 create_command('DeeplTranslateToEn', function(opts)
   helper.deepl_translate(opts.count, opts.line1, opts.line2, 'EN', 'JA', { 'yank', 'echo' })
-end, { range = '%', bar = true })
+end, { range = '%' })
 
 create_command('DeeplTranslateToJa', function(opts)
   helper.deepl_translate(opts.count, opts.line1, opts.line2, 'JA', 'EN', { 'yank', 'echo' })
-end, { range = '%', bar = true })
+end, { range = '%' })
 
 create_command('DeeplTranslateToEnOpenBuffer', function(opts)
   helper.deepl_translate(opts.count, opts.line1, opts.line2, 'EN', 'JA', { 'yank', 'buffer' })
-end, { range = '%', bar = true })
+end, { range = '%' })
 
 create_command('DeeplTranslateToJaOpenBuffer', function(opts)
   helper.deepl_translate(opts.count, opts.line1, opts.line2, 'JA', 'EN', { 'yank', 'buffer' })
-end, { range = '%', bar = true })
+end, { range = '%' })
 
 -- Helper for claudecode.nvim
 create_command('ClaudeCodeAtGitRoot', function(opts)
@@ -224,10 +229,8 @@ end, {
     Useful when wanting to use a conversation related with gir-root.
     e.g., --continue, --resume.
   ]],
-  bar = true,
 })
 
--- LuaSnipEdit コマンド（修正版）
 create_command('LuaSnipEdit', function(opts)
   local filetype = opts.args ~= '' and opts.args or vim.bo.filetype
   if filetype == '' then
@@ -241,11 +244,9 @@ create_command('LuaSnipEdit', function(opts)
   )
   vim.cmd('edit ' .. vim.fn.fnameescape(luasnip_path))
 end, {
-  nargs = '?',  -- 修正: args → nargs
+  nargs = '?',
   desc = 'Edit LuaSnip snippet file for current or specified filetype',
-  complete = function()
-    return vim.fn.getcompletion('', 'filetype')
-  end
+  complete = 'filetype',
 })
 
 ---Tapis functions
