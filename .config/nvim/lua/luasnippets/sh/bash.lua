7e8c9008d73765dc2e75e13d567a5b118a41acf1
aiya000 <aiya000.develop@gmail.com>
2025-09-26 00:26:04 +0900

    fix(Neovim): Resolve errors of luasnippetes/sh [stash:1]

--- .config/nvim/lua/luasnippets/sh/bash.lua
+++ .config/nvim/lua/luasnippets/sh/bash.lua
@@ -1,23 +1,17 @@
-local ls = require('luasnip')
-local s = ls.snippet
-local t = ls.text_node
-local i = ls.insert_node
 local fmt = require('luasnip.extras.fmt').fmt
-local sm = ls.extend_decorator.apply(s, {}, {
-  common = {},
-  condition = function()
-    return true
-  end,
-})
+local list = require('utils.list')
+local ls = require('luasnip')
+local utils = require('utils.luasnip')
 
-return {
+local sm = utils.sm
+local s = utils.s
+local i = ls.insert_node
+local t = ls.text_node
+
+return list.concat(
   -- function definition
   sm(
-    {
-      { trig = 'function', dscr = 'bash function' },
-      { trig = 'func', dscr = 'bash function' },
-      { trig = 'fun', dscr = 'bash function' },
-    },
+    { 'function', 'func', 'fun' },
     fmt(
       [[
 function {} () {{
@@ -31,78 +25,77 @@ function {} () {{
   ),
 
   -- test operations
-  s(
-    'test',
-    fmt('[[ {} ]]', {
-      i(0),
-    })
-  ),
+  {
+    s(
+      'test',
+      fmt('[[ {} ]]', {
+        i(0),
+      })
+    ),
 
-  s(
-    'test_regex',
-    fmt('[[ {} =~ {} ]]', {
-      i(1),
-      i(0),
-    })
-  ),
+    s(
+      'test_regex',
+      fmt('[[ {} =~ {} ]]', {
+        i(1),
+        i(0),
+      })
+    ),
 
-  -- variable replacement operations
-  s(
-    'var_replace_only_first',
-    fmt('${{${{{}}}/${{{}}}/${{{}}}}}{}', {
-      i(1, 'varname'),
-      i(2, 'pattern_str'),
-      i(3, 'replaced'),
-      i(0),
-    })
-  ),
+    -- variable replacement operations
+    s(
+      'var_replace_only_first',
+      fmt([[${{{1}/{2}/{3}}}{4}]], {
+        i(1, 'varname'),
+        i(2, 'pattern_str'),
+        i(3, 'replaced'),
+        i(0),
+      })
+    ),
 
-  s(
-    'var_replace_all',
-    fmt('${{${{{}}}//{}/${{{}}}}}{}', {
-      i(1, 'varname'),
-      i(2, 'pattern_str'),
-      i(3, 'replaced'),
-      i(0),
-    })
-  ),
+    s(
+      'var_replace_all',
+      fmt([[${{{}//{}/${{{}}}}}{}]], {
+        i(1, 'varname'),
+        i(2, 'pattern_str'),
+        i(3, 'replaced'),
+        i(0),
+      })
+    ),
 
-  s(
-    'var_replace_head',
-    fmt('${{${{{}}}/#{}/${{{}}}}}{}', {
-      i(1, 'varname'),
-      i(2, 'pattern_str'),
-      i(3, 'replaced'),
-      i(0),
-    })
-  ),
+    s(
+      'var_replace_head',
+      fmt([[${{{}/#{}/${{{}}}}}{}]], {
+        i(1, 'varname'),
+        i(2, 'pattern_str'),
+        i(3, 'replaced'),
+        i(0),
+      })
+    ),
 
-  s(
-    'var_replace_tail',
-    fmt('${{${{{}}}/%{}/${{{}}}}}{}', {
-      i(1, 'varname'),
-      i(2, 'pattern_str'),
-      i(3, 'replaced'),
-      i(0),
-    })
-  ),
+    s(
+      'var_replace_tail',
+      fmt([[${{{}/%{}/${{{}}}}}{}]], {
+        i(1, 'varname'),
+        i(2, 'pattern_str'),
+        i(3, 'replaced'),
+        i(0),
+      })
+    ),
 
-  s(
-    'var_or_default',
-    fmt('${{{}:-{}}}{}', {
-      i(1, 'varname'),
-      i(2, 'default_value'),
-      i(0),
-    })
-  ),
+    s(
+      'var_or_default',
+      fmt([[${{{1}:-{2}}}{}]], {
+        i(1, 'varname'),
+        i(2, 'default_value'),
+        i(0),
+      })
+    ),
+  },
 
   -- array slicing
   sm(
-    {
-      { trig = 'array_slice', dscr = 'array slice' },
-      { trig = 'slice', dscr = 'array slice' },
-    },
-    fmt('${{{}[@]:{}:{}}}{}', {
+    { 'array_slice', 'slice' },
+    fmt([[${{{1}[@]:{2}:{3}}}{}]], {
       i(1, 'array'),
       i(2, 'start'),
       i(3, 'end'),
@@ -110,14 +103,16 @@ function {} () {{
     })
   ),
 
-  s(
-    'assign_sliced_array',
-    fmt('{}=("${{{}[@]:{}:{}}}"){}', {
-      i(1, 'new_array'),
-      i(2, 'old_array'),
-      i(3, 'start'),
-      i(4, 'end'),
-      i(0),
-    })
-  ),
-}
+  {
+    s(
+      'assign_sliced_array',
+      fmt([[{}=("${{{2}[@]:{3}:{4}}}"){}]], {
+        i(1, 'new_array'),
+        i(2, 'old_array'),
+        i(3, 'start'),
+        i(4, 'end'),
+        i(0),
+      })
+    ),
+  }
+)
--- .config/nvim/lua/luasnippets/sh/sh.lua
+++ .config/nvim/lua/luasnippets/sh/sh.lua
@@ -1,161 +1,146 @@
-local ls = require('luasnip')
-local s = ls.snippet
-local t = ls.text_node
-local i = ls.insert_node
 local fmt = require('luasnip.extras.fmt').fmt
-local sm = ls.extend_decorator.apply(s, {}, {
-  common = {},
-  condition = function()
-    return true
-  end,
-})
+local list = require('utils.list')
+local ls = require('luasnip')
+local utils = require('utils.luasnip')
 
-return {
-  -- shebangs
-  sm({
-    { trig = 'bash_shebang', dscr = 'bash shebang' },
-    { trig = 'shebang', dscr = 'bash shebang' },
-    { trig = 'shebang_bash', dscr = 'bash shebang' },
-  }, {
-    t('#!/bin/bash'),
-  }),
+local sm = utils.sm
+local s = utils.s
+local i = ls.insert_node
+local t = ls.text_node
 
-  sm({
-    { trig = 'bats_shebang', dscr = 'bats shebang' },
-    { trig = 'shebang_bats', dscr = 'bats shebang' },
-  }, {
-    t('#!/usr/bin/env bats'),
-  }),
+return list.concat(
+    -- shebangs
+    sm(
+      { 'bash_shebang', 'shebang', 'shebang_bash' },
+      t('#!/bin/bash')
+    ),
 
-  -- syntaxes
-  s(
-    'if',
-    fmt(
-      [[
-if {} ; then
-  {}
-fi]],
-      {
-        i(1, '#:condtion'),
-        i(0),
-      }
-    )
-  ),
+    sm(
+      { 'bats_shebang', 'shebang_bats' },
+      t('#!/usr/bin/env bats')
+    ),
 
-  s(
-    'else',
-    fmt(
-      [[
-else
-  {}]],
-      {
-        i(0, 'TARGET'),
-      }
-    )
-  ),
+    -- syntaxes
+    {
+      s(
+        'if',
+        fmt([[
+          if {} ; then
+            {}
+          fi
+        ]], {
+          i(1, ''),
+          i(2, ''),
+        })
+      ),
 
-  s(
-    'elif',
-    fmt(
-      [[
-elif {} ; then
-  {}]],
-      {
-        i(1, '#:condition'),
-        i(0, 'TARGET'),
-      }
-    )
-  ),
+      s(
+        'else',
+        fmt([[else{}]], {
+          i(0, ''),
+        })
+      ),
 
-  s(
-    'for',
-    fmt(
-      [[
+      s(
+        'elif',
+        fmt([[
+          elif {} ; then
+            {}
+        ]], {
+          i(1, ''),
+          i(2, ''),
+        })
+      ),
+
+      s(
+        'for',
+        fmt(
+          [[
 for {} in {} ; do
   {}
 done]],
-      {
-        i(1, 'x'),
-        i(2, '#:var'),
-        i(0),
-      }
-    )
-  ),
+          {
+            i(1, 'x'),
+            i(2, '#:var'),
+            i(0),
+          }
+        )
+      ),
 
-  s(
-    'for_index',
-    fmt(
-      [[
+      s(
+        'for_index',
+        fmt(
+          [[
 for (( {}; {}; {} )) ; do
   {}
 done]],
-      {
-        i(1, '#:init'),
-        i(2, '#:condtion'),
-        i(3, '#:effect'),
-        i(0),
-      }
-    )
-  ),
+          {
+            i(1, '#:init'),
+            i(2, '#:condtion'),
+            i(3, '#:effect'),
+            i(0),
+          }
+        )
+      ),
 
-  s(
-    'while',
-    fmt(
-      [[
+      s(
+        'while',
+        fmt(
+          [[
 while {} ; do
   {}
 done]],
-      {
-        i(1, '#:condition'),
-        i(0, 'TARGET'),
-      }
-    )
-  ),
+          {
+            i(1, '#:condition'),
+            i(0, 'TARGET'),
+          }
+        )
+      ),
 
-  s(
-    'case',
-    fmt(
-      [[
+      s(
+        'case',
+        fmt(
+          [[
 case {} in
   {}
 esac]],
-      {
-        i(1),
-        i(0),
-      }
-    )
-  ),
+          {
+            i(1),
+            i(0),
+          }
+        )
+      ),
 
-  s(
-    'case_pattern',
-    fmt(
-      [[
+      s(
+        'case_pattern',
+        fmt(
+          [[
 {})
   {}
 ;;]],
-      {
-        i(1),
-        i(0),
-      }
-    )
-  ),
+          {
+            i(1),
+            i(0),
+          }
+        )
+      ),
 
-  s(
-    'case_pattern_wildcard',
-    fmt(
-      [[
+      s(
+        'case_pattern_wildcard',
+        fmt(
+          [[
 *)
   {}]],
-      {
-        i(0),
-      }
-    )
-  ),
+          {
+            i(0),
+          }
+        )
+      ),
 
-  s(
-    'template_case',
-    fmt(
-      [[
+      s(
+        'template_case',
+        fmt(
+          [[
 case {} in
 {})
   {}
@@ -163,181 +148,172 @@ case {} in
 *)
   {}
 esac]],
-      {
-        i(1),
-        i(2, 'pattern'),
-        i(3),
-        i(0, 'echo "$$1 Didn\'t match anything"'),
-      }
-    )
-  ),
-
-  -- array operations
-  sm(
-    {
-      { trig = 'array_length', dscr = 'array length' },
-      { trig = 'array_size', dscr = 'array length' },
+          {
+            i(1),
+            i(2, 'pattern'),
+            i(3),
+            i(0, 'echo "$$1 Didn\'t match anything"'),
+          }
+        )
+      ),
     },
-    fmt('${{#${{{}}}}}{}', {
-      i(1, 'array_name'),
-      i(0),
-    })
-  ),
 
-  s(
-    'array_whole',
-    fmt('${{${{{}}}[@]}}{}', {
-      i(1, 'array_name'),
-      i(0),
-    })
-  ),
+    -- array operations
+    sm(
+      { 'array_length', 'array_size' },
+      fmt('${{#${{{}}}}}{}', {
+        i(1, 'array_name'),
+        i(0),
+      })
+    ),
 
-  s(
-    'array_index',
-    fmt('${{${{{}}}[{}]}}', {
-      i(1, 'array_name'),
-      i(2, '0'),
-    })
-  ),
-
-  sm(
     {
-      { trig = 'array_append', dscr = 'array append' },
-      { trig = 'array_push', dscr = 'array append' },
-      { trig = 'array_add', dscr = 'array append' },
-    },
-    fmt('{}+=("{}")', {
-      i(1, 'array'),
-      i(0),
-    })
-  ),
+      s(
+        'array_whole',
+        fmt('${{${{{}}}[@]}}{}', {
+          i(1, 'array_name'),
+          i(0),
+        })
+      ),
 
-  s(
-    'array_join',
-    fmt(
-      [[
+      s(
+        'array_index',
+        fmt('${{${{{}}}[{}]}}', {
+          i(1, 'array_name'),
+          i(2, '0'),
+        })
+      ),
+    },
+
+    sm(
+      { 'array_append', 'array_push', 'array_add' },
+      fmt('{}+=("{}")', {
+        i(1, 'array'),
+        i(0),
+      })
+    ),
+
+    {
+      s(
+        'array_join',
+        fmt(
+          [[
 {}="$(printf "%s{}" "${{{}[@]}}")"
 {}=${{{}%{}}}{}]],
-      {
-        i(1, 'new_array'),
-        i(2, 'separator'),
-        i(3, 'old_array'),
-        i(4),
-        i(5),
-        i(6),
-        i(0),
-      }
-    )
-  ),
+          {
+            i(1, 'new_array'),
+            i(2, 'separator'),
+            i(3, 'old_array'),
+            i(4),
+            i(5),
+            i(6),
+            i(0),
+          }
+        )
+      ),
+    },
 
-  -- arguments
-  sm({
-    { trig = 'args_length', dscr = 'arguments length' },
-    { trig = 'args_size', dscr = 'arguments length' },
-  }, {
-    t('$#'),
-  }),
+    -- arguments
+    sm(
+      { 'args_length', 'args_size' },
+      t('$#')
+    ),
 
-  s('args_whole', {
-    t('$@'),
-  }),
+    {
+      s('args_whole', t('$@')),
+    },
 
-  -- templates
-  s(
-    'array_from_string',
-    fmt('IFS=\'{}\' read -ra {} <<< "{}"{}', {
-      i(2, ' '),
-      i(1, 'var_name'),
-      i(3, 'str_split_by_ifs'),
-      i(0),
-    })
-  ),
+    -- templates
+    {
+      s(
+        'array_from_string',
+        fmt(
+          [[
+            IFS='{delim}' read -ra {var} <<< "{str_split_by_ifs}"
+          ]],
+          {
+            delim = i(1, ' '),
+            var = i(2, 'var_name'),
+            str_split_by_ifs = i(3, 'str_split_by_ifs'),
+          }
+        )
+      ),
 
-  s(
-    'array_from_cmd_lines',
-    fmt(
-      [[
+      s(
+        'array_from_cmd_lines',
+        fmt(
+          [[
 {}=()
 while IFS='' read -r line ; do
-  {}+=("$line") ;
+  {}+=(\"$line\") ;
 done < <({}){}]],
-      {
-        i(1, 'array'),
-        i(2),
-        i(3, 'cmd'),
-        i(0),
-      }
-    )
-  ),
-
-  sm(
-    {
-      { trig = 'read_lines_from_pipe', dscr = 'read lines from pipe' },
-      { trig = 'while_lines', dscr = 'read lines from pipe' },
+          {
+            i(1, 'array'),
+            i(2),
+            i(3, 'cmd'),
+            i(0),
+          }
+        )
+      ),
     },
-    fmt(
-      [[
+
+    sm(
+      { 'read_lines_from_pipe', 'while_lines' },
+      fmt(
+        [[
 while read -r {} ; do
   {}
 done]],
-      {
-        i(1, 'line'),
-        i(0),
-      }
-    )
-  ),
+        {
+          i(1, 'line'),
+          i(0),
+        }
+      )
+    ),
 
-  sm(
+    sm(
+      { 'prompt', 'prompt_for_input', 'ask_for_input' },
+      fmt([[read -rp '{}' {}]], {
+        i(1, 'message'),
+        i(0, 'var'),
+      })
+    ),
+
+    sm(
+      { 'which', 'command_v' },
+      fmt([[command -v {} &> /dev/null]], {
+        i(1, 'cmd'),
+      })
+    ),
+
+    sm(
+      { 'define_sourced_dir', 'define_this_script_dir' },
+      fmt([[{}="$(cd -- "$(dirname -- "${{BASH_SOURCE:-\$0}}")" && pwd || exit 1)"]], {
+        i(0, 'this_script_dir'),
+      })
+    ),
+
     {
-      { trig = 'prompt', dscr = 'prompt for input' },
-      { trig = 'prompt_for_input', dscr = 'prompt for input' },
-      { trig = 'ask_for_input', dscr = 'prompt for input' },
-    },
-    fmt("read -rp '{}' {}", {
-      i(1, 'message'),
-      i(0, 'var'),
-    })
-  ),
+      s(
+        'assign_array',
+        fmt('{}=("{}")', {
+          i(1, 'new_array'),
+          i(0, 'source_array'),
+        })
+      ),
 
-  sm(
-    {
-      { trig = 'which', dscr = 'command exists check' },
-      { trig = 'command_v', dscr = 'command exists check' },
-    },
-    fmt('command -v {} &> /dev/null', {
-      i(1, 'cmd'),
-    })
-  ),
+      s(
+        'export_function',
+        fmt('export -f {}', {
+          i(0, 'func_name'),
+        })
+      ),
 
-  sm(
-    {
-      { trig = 'define_sourced_dir', dscr = 'define script directory' },
-      { trig = 'define_this_script_dir', dscr = 'define script directory' },
-    },
-    fmt('{}="$(cd -- "$(dirname -- "${{BASH_SOURCE:-\\$0}}")" && pwd || exit 1)"', {
-      i(0, 'this_script_dir'),
-    })
-  ),
-
-  s(
-    'assign_array',
-    fmt('{}=("{}")', {
-      i(1, 'new_array'),
-      i(0, 'source_array'),
-    })
-  ),
-
-  s(
-    'export_function',
-    fmt('export -f {}', {
-      i(0, 'func_name'),
-    })
-  ),
-
-  s(
-    'poi',
-    fmt('echo "poi: ${{{}}}"', {
-      i(0, 'var_name'),
-    })
-  ),
-}
+      s(
+        'poi',
+        fmt([[echo "poi: ${{{}}}"]], {
+          i(0, 'var_name'),
+        })
+      ),
+    }
+)
