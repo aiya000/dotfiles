3dbacccf9b139d1aa9720b01225ae9c48c7bbae0
aiya000 <aiya000.develop@gmail.com>
2025-09-27 23:45:18 +0900

    feat(shell): Add gpw alias [stash:1]

--- .sh_generic/aliases.sh
+++ .sh_generic/aliases.sh
@@ -398,6 +398,8 @@ if i_have git ; then
     git-push-wip
   }
 
+  alias gpw=git-push-wip-force
+
   # Set casual user.name and user.email at local
   alias git-set-casual-name='git config --local user.name aiya000 && git config --local user.email aiya000.develop@gmail.com ; git config --local user.name ; git config --local user.email'
   alias cdg=cd-to-git-root
