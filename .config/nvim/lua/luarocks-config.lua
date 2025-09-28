7572deacc533712f7e10fba96b994fcdb6dfac6a
aiya000 <aiya000.develop@gmail.com>
2025-09-29 01:23:44 +0900

    feat(Neovim): Add configuration for luarocks with Lua-5.1

--- .config/nvim/lua/luarocks-config.lua
+++ .config/nvim/lua/luarocks-config.lua
@@ -1,3 +1,14 @@
--- Enable packages installed by `luarocks install --local`
-package.path = package.path .. ';' .. os.getenv('HOME') .. '/.luarocks/share/lua/5.2/?.lua'
-package.path = package.path .. ';' .. os.getenv('HOME') .. '/.luarocks/share/lua/5.2/?/init.lua'
+---Enables packages installed by `luarocks install --local`
+
+local s = require('utils.functions').s
+
+package.path = s('{path};{home}/.luarocks/share/lua/5.1/?.lua', {
+  path = package.path,
+  home = vim.env.HOME,
+})
+
+-- 一応Lua-5.2の環境用にも設定しておく
+package.path = s('{path};{home}/.luarocks/share/lua/5.2/?.lua', {
+  path = package.path,
+  home = vim.env.HOME,
+})
