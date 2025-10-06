e7bf00b859820ee7bf5c4e9117a606d822aa17d2
aiya000 <aiya000.develop@gmail.com>
2025-10-06 21:35:08 +0900

    feat(Neovim > ftplugin/git-log): Add normal-mode R keymap to run `git-rebase`

--- .config/nvim/after/ftplugin/git-log.lua
+++ .config/nvim/after/ftplugin/git-log.lua
@@ -1,7 +1,7 @@
 -- This filetype defined by plugin/gitlog.vim
 
-local list = require('utils.list')
 local git_log = require('git-log')
+local list = require('utils.list')
 
 vim.opt_local.list = false
 vim.opt_local.cul = true
@@ -30,3 +30,21 @@ vim.keymap.set('n', '<C-r>', function()
 end, { buffer = true, silent = true })
 
 vim.keymap.set('n', 'S', open_git_show, { buffer = true, silent = true })
+
+if list.has(vim.b.gitlog_args or {}, '--oneline') then
+  local function rebase_interactive_autosquash_to_current_line_revision()
+    local line = vim.api.nvim_get_current_line()
+    local revision = vim.split(line, ' ')[1]
+    if revision == nil then
+      vim.notify('Invalid line:\n' .. line, vim.log.levels.ERROR)
+      return
+    end
+
+    vim.cmd.tabnew()
+    vim.fn.jobstart({ 'git', 'rebase', '--interactive', '--autosquash', revision }, {
+      term = true,
+    })
+  end
+
+  vim.keymap.set('n', 'R', rebase_interactive_autosquash_to_current_line_revision, { buffer = true })
+end
