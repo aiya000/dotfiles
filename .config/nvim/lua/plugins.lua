344a7e7e7d01315b915e5671f7a480195acd194b
aiya000 <aiya000.develop@gmail.com>
2025-10-06 21:47:30 +0900

    feat(Neovim): Improve incline.nvim behaviors and styles

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2045,19 +2045,27 @@ return {
           padding = 0,
           margin = { horizontal = 0, vertical = 0 },
         },
+
         render = function(props)
+          if vim.bo.filetype == 'gitcommit' then
+            return {}
+          end
+
           local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
           if filename == '' then
             filename = '[No Name]'
           end
+
           local ft_icon, ft_color = devicons.get_icon_color(filename)
           local modified = vim.bo[props.buf].modified
           local res = {
             ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
             ' ',
             { filename, gui = modified and 'bold,italic' or 'bold' },
-            guibg = '#44406e',
+            guibg = '#afafff',
+            guifg = '#000000',
           }
+
           if props.focused then
             for _, item in ipairs(navic.get_data(props.buf) or {}) do
               table.insert(res, {
@@ -2067,6 +2075,7 @@ return {
               })
             end
           end
+
           table.insert(res, ' ')
           return res
         end,
