9d58b206dc45feec2be32f92ec9be4cd56967537
aiya000 <aiya000.develop@gmail.com>
2025-10-07 05:32:33 +0900

    fix(Neovim > ftplugin/gitcommit): Resolve setting cursor to top
    いきなり2行目から始まるようになったのなんで？

--- .config/nvim/after/ftplugin/gitcommit.lua
+++ .config/nvim/after/ftplugin/gitcommit.lua
@@ -2,4 +2,6 @@ vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 vim.opt_local.textwidth = 0
 
-vim.api.nvim_win_set_cursor(0, { 1, 0 })
+vim.schedule(function()
+  vim.api.nvim_win_set_cursor(0, { 1, 0 })
+end, 0)
