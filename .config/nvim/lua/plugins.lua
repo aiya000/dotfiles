6073f714cabfa824db70e348aece2515337bf209
aiya000 <aiya000.develop@gmail.com>
2025-10-06 10:24:32 +0900

    refactor(Neovim > plugins): Fix indentation in dial.nvim config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1908,52 +1908,52 @@ return {
   -- }}}
   -- dial.nvim {{{
 
-    {
-      'monaqa/dial.nvim',
-      config = function()
-        vim.keymap.set('n', '<C-a>', function()
-          require('dial.map').manipulate('increment', 'normal')
-        end)
+  {
+    'monaqa/dial.nvim',
+    config = function()
+      vim.keymap.set('n', '<C-a>', function()
+        require('dial.map').manipulate('increment', 'normal')
+      end)
 
-        vim.keymap.set('n', '<C-x>', function()
-          require('dial.map').manipulate('decrement', 'normal')
-        end)
+      vim.keymap.set('n', '<C-x>', function()
+        require('dial.map').manipulate('decrement', 'normal')
+      end)
 
-        vim.keymap.set('n', 'g<C-a>', function()
-          require('dial.map').manipulate('increment', 'gnormal')
-        end)
+      vim.keymap.set('n', 'g<C-a>', function()
+        require('dial.map').manipulate('increment', 'gnormal')
+      end)
 
-        vim.keymap.set('n', 'g<C-x>', function()
-          require('dial.map').manipulate('decrement', 'gnormal')
-        end)
+      vim.keymap.set('n', 'g<C-x>', function()
+        require('dial.map').manipulate('decrement', 'gnormal')
+      end)
 
-        vim.keymap.set('x', '<C-a>', function()
-          require('dial.map').manipulate('increment', 'visual')
-        end)
+      vim.keymap.set('x', '<C-a>', function()
+        require('dial.map').manipulate('increment', 'visual')
+      end)
 
-        vim.keymap.set('x', '<C-x>', function()
-          require('dial.map').manipulate('decrement', 'visual')
-        end)
+      vim.keymap.set('x', '<C-x>', function()
+        require('dial.map').manipulate('decrement', 'visual')
+      end)
 
-        vim.keymap.set('x', 'g<C-a>', function()
-          require('dial.map').manipulate('increment', 'gvisual')
-        end)
+      vim.keymap.set('x', 'g<C-a>', function()
+        require('dial.map').manipulate('increment', 'gvisual')
+      end)
 
-        vim.keymap.set('x', 'g<C-x>', function()
-          require('dial.map').manipulate('decrement', 'gvisual')
-        end)
+      vim.keymap.set('x', 'g<C-x>', function()
+        require('dial.map').manipulate('decrement', 'gvisual')
+      end)
 
-        local augend = require('dial.augend')
-        require('dial.config').augends:register_group{
-          default = {
-            augend.integer.alias.decimal,
-            augend.integer.alias.hex,
-            augend.date.alias['%Y/%m/%d'],
-            augend.constant.alias.bool,
-          },
-        }
-      end
-    },
+      local augend = require('dial.augend')
+      require('dial.config').augends:register_group{
+        default = {
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.date.alias['%Y/%m/%d'],
+          augend.constant.alias.bool,
+        },
+      }
+    end
+  },
 
   -- }}}
 }
