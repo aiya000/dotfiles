3cfdf3dccc79e17306c8cc9d228b68b1357a6167
aiya000 <aiya000.develop@gmail.com>
2025-10-09 05:41:46 +0900

    refactor(Neovim): Move `helper.setup_operator_surround()` to plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1082,19 +1082,87 @@ return {
     end,
   },
 
-  -- }}}
-  -- vim-operator-user {{{
-
-  { 'kana/vim-operator-user' },
-
-  -- }}}
+   -- }}}
   -- vim-operator-surround {{{
 
   {
     'rhysd/vim-operator-surround',
     dependencies = { 'kana/vim-operator-user' },
     config = function()
-      vim.schedule(helper.setup_operator_surround)
+      vim.schedule(function()
+        -- Basic symbols excluding brackets () [] {} and ` for unique mappings
+        local common_symbol_blocks =
+          vim.iter(list.concat(list.char_range('!', "'"), { '*', '&', '_', '|', '~', ':', '/' }))
+            :map(function(sym)
+              return {
+                block = { sym, sym },
+                motionwise = { 'char', 'line', 'block' },
+                keys = { sym },
+              }
+            end)
+            :totable()
+        local common_blocks = list.concat(common_symbol_blocks, {
+          -- English
+          { block = { '(', ')' }, motionwise = { 'char', 'line', 'block' }, keys = { '(', ')', 'p' } },
+          { block = { '[', ']' }, motionwise = { 'char', 'line', 'block' }, keys = { ']', 'k' } },
+          { block = { '{', '}' }, motionwise = { 'char', 'line', 'block' }, keys = { '{', '}', 'P' } },
+          { block = { '<', '>' }, motionwise = { 'char', 'line', 'block' }, keys = { '<', '>', 'K' } },
+          { block = { ' ', ' ' }, motionwise = { 'char', 'line', 'block' }, keys = { '  ' } },
+          { block = { '`', '`' }, motionwise = { 'char', 'line', 'block' }, keys = { '`', 'b' } },
+          -- 日本語
+          { block = { '（', '）' }, motionwise = { 'char', 'line', 'block' }, keys = { 'jp' } },
+          { block = { '「', '」' }, motionwise = { 'char', 'line', 'block' }, keys = { 'jk' } },
+          { block = { '【', '】' }, motionwise = { 'char', 'line', 'block' }, keys = { 'jK' } },
+          { block = { '『', '』' }, motionwise = { 'char', 'line', 'block' }, keys = { 'j-k' } },
+        })
+
+        local common_markdown_blocks = {
+          { block = { '**', '**' }, motionwise = { 'char', 'block' }, keys = { 'B' } },
+          { block = { '~~', '~~' }, motionwise = { 'char', 'block' }, keys = { '~' } },
+          -- { block = { '__', '__' }, motionwise = { 'char', 'block'  } } -- あんまり使わないから定義しないでおく
+          { block = { '```', '```' }, motionwise = { 'line', 'block' }, keys = { '```' } },
+        }
+
+        local html_blocks = {
+          { block = { '<p>', '</p>' }, motionwise = { 'char' }, keys = { '[p' } },
+          { block = { '<a>', '</a>' }, motionwise = { 'char' }, keys = { '[a' } },
+          { block = { '<div>', '</div>' }, motionwise = { 'char' }, keys = { '[d' } },
+          { block = { '<span>', '</span>' }, motionwise = { 'char' }, keys = { '[s' } },
+          { block = { '<h1>', '</h1>' }, motionwise = { 'char' }, keys = { '[h1' } },
+          { block = { '<h2>', '</h2>' }, motionwise = { 'char' }, keys = { '[h2' } },
+          { block = { '<h3>', '</h3>' }, motionwise = { 'char' }, keys = { '[h3' } },
+          { block = { '<h4>', '</h4>' }, motionwise = { 'char' }, keys = { '[h4' } },
+          { block = { '<h5>', '</h5>' }, motionwise = { 'char' }, keys = { '[h5' } },
+          { block = { '<ol>', '</ol>' }, motionwise = { 'char' }, keys = { '[ol' } },
+          { block = { '<ul>', '</ul>' }, motionwise = { 'char' }, keys = { '[ul' } },
+          { block = { '<li>', '</li>' }, motionwise = { 'char' }, keys = { '[li' } },
+        }
+
+        vim.g['operator#surround#blocks'] = {
+          ['-'] = common_blocks,
+          markdown = list.concat(common_markdown_blocks, html_blocks, {
+            { block = { '<b>', '</b>' }, motionwise = { 'char' }, keys = { '[b' } }, -- HTMLではご法度なのでこっち（README.mdとかで使う）
+          }),
+          html = html_blocks,
+          lua = {
+            { block = { '[[', ']]' }, motionwise = { 'char', 'line', 'block' }, keys = { '[', ']' } },
+          },
+          review = {
+            { block = { '@<b>{', '}' }, motionwise = { 'char' }, keys = { 'B' } },
+            { block = { '@<i>{', '}' }, motionwise = { 'char' }, keys = { 'i' } },
+            { block = { '@<u>{', '}' }, motionwise = { 'char' }, keys = { 'u' } },
+            { block = { '@<tt>{', '}' }, motionwise = { 'char' }, keys = { 't' } },
+            { block = { '@<idx>{', '}' }, motionwise = { 'char' }, keys = { 'x' } },
+            { block = { '@<ruby>{', ', ruby}' }, motionwise = { 'char' }, keys = { 'r' } },
+            { block = { '@<code>{', '}' }, motionwise = { 'char' }, keys = { 'c' } },
+            { block = { '@<mathcode>{', '}' }, motionwise = { 'char' }, keys = { 'm' } },
+            { block = { '@<img>{', '}' }, motionwise = { 'char' }, keys = { '[i' } },
+            { block = { '@<list>{', '}' }, motionwise = { 'char' }, keys = { '[l' } },
+          },
+          vue = html_blocks,
+          ['typescript.tsx'] = html_blocks,
+        }
+      end)
     end,
   },
 
