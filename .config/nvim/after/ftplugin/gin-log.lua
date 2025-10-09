e028b81364ff7c234c7368b4baf14ba71193a589
aiya000 <aiya000.develop@gmail.com>
2025-10-10 00:04:15 +0900

    feat(Neovim > ftplugin/gin-log): Simply do yank selected text or current line

--- .config/nvim/after/ftplugin/gin-log.lua
+++ .config/nvim/after/ftplugin/gin-log.lua
@@ -1,3 +1,11 @@
 vim.keymap.set('n', 'Q', function()
   vim.api.nvim_buf_delete(0, { force = true })
 end, { buffer = true, silent = true })
+
+vim.keymap.set('n', 'yy', function()
+  vim.cmd('normal! yy')
+end, { buffer = true })
+
+vim.keymap.set('v', 'y', function()
+  vim.cmd('normal! y')
+end, { buffer = true })
