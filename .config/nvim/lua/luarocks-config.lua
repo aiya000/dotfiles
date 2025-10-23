3c1b3f03dc202897f28b119b80e874bff7192b09
aiya000 <aiya000.develop@gmail.com>
2025-10-24 05:07:57 +0900

    refactor(Neovim): Simplify luarocks-config by using string.format instead of utils.functions
    Replace custom string interpolation function with Lua's standard string.format,
    reducing dependencies and improving code clarity.

--- .config/nvim/lua/luarocks-config.lua
+++ .config/nvim/lua/luarocks-config.lua
@@ -1,14 +1,11 @@
 ---Enables packages installed by `luarocks install --local`
 
-local s = require('utils.functions').s
-
-package.path = s('{path};{home}/.luarocks/share/lua/5.1/?.lua', {
-  path = package.path,
-  home = vim.env.HOME,
-})
-
--- 一応Lua-5.2の環境用にも設定しておく
-package.path = s('{path};{home}/.luarocks/share/lua/5.2/?.lua', {
-  path = package.path,
-  home = vim.env.HOME,
-})
+-- 環境によってlua5.1だったりlua5.2だったりするので、全部追加しておく
+package.path = ('%s;%s/.luarocks/share/lua/5.1/?.lua'):format(
+  package.path,
+  vim.env.HOME
+)
+package.path = ('%s;%s/.luarocks/share/lua/5.2/?.lua'):format(
+  package.path,
+  vim.env.HOME
+)
