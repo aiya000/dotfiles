99ff8cec99fcae68d50e94e18d151c88a75b4f31
aiya000 <aiya000.develop@gmail.com>
2025-10-27 05:43:46 +0900

    feat(Neovim > snips): Create snips for 'help' filetype

---
+++ .config/nvim/lua/luasnippets/help.lua
@@ -0,0 +1,19 @@
+local fmt = require('luasnip.extras.fmt').fmt
+local list = require('utils.list')
+local ls = require('luasnip')
+local utils = require('utils.luasnip')
+
+local sm = utils.sm
+local s = ls.snippet
+local i = ls.insert_node
+local t = ls.text_node
+
+return {
+  snippets = list.concat(
+    {
+      s('bar', fmt([[==================]], {})),
+      s('subbar', fmt([[------------]], {})),
+    }
+  ),
+  autosnippets = {},
+}
