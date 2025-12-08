668f104f6d5d84672a9ebdf05f247cf3282ad936
aiya000 <aiya000.develop@gmail.com>
2025-12-08 18:19:43 +0900

    change(Shell): Replace `poweroff` & `sudoedit` aliases with improved `ps` alias
    Replaced two rarely-used aliases with a more practical process display alias
    that shows PID, start time, and command sorted by start time for better system
    monitoring.

--- .sh_generic/macOS.sh
+++ .sh_generic/macOS.sh
@@ -2,8 +2,7 @@
 export BROWSER=open
 
 alias x=open
-alias poweroff='sudo shutdown -h now'
-alias sudoedit='sudo -e'
+alias ps='ps -e -o pid,lstart,command | sort -k2,6'  # Show pid, started time, and the command. Sort by start time
 alias docker-force-remove-containers-data='rm -rf ~/Library/Containers/com.docker.docker/Data/*'
 
 i_have gsed && alias sed=gsed
