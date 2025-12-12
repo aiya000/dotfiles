05aebd48e1c0c93448ec73fcabbfd933cad15299
aiya000 <aiya000.develop@gmail.com>
2025-12-12 10:04:35 +0900

    feat(Neovim > plugins): Enhance kensaku-search.vim with dependency & keymapping
    - Add kensaku.vim as dependency
    - Configure <CR> keymapping for kensaku-search-replace in command mode

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1038,7 +1038,13 @@ return {
   -- }}}
   -- kensaku-search.vim {{{
 
-  { 'lambdalisue/kensaku-search.vim' },
+  {
+    'lambdalisue/kensaku-search.vim',
+    dependencies = { 'lambdalisue/kensaku.vim' },
+    config = function()
+      vim.keymap.set('c', '<CR>', '<Plug>(kensaku-search-replace)<CR>')
+    end,
+  },
 
   -- }}}
   -- yanky.nvim {{{
