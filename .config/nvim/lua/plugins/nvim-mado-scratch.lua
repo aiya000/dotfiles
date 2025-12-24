e38100671e23c7e0a5fe5b1613192b332906d194
aiya000 <aiya000.develop@gmail.com>
2025-12-24 21:55:18 +0900

    change(Neovim > plugins > nvim-mado-scratch): Switch back to main repository from local development branch

--- .config/nvim/lua/plugins/nvim-mado-scratch.lua
+++ .config/nvim/lua/plugins/nvim-mado-scratch.lua
@@ -1,16 +1,14 @@
 return {
-  -- 'aiya000/nvim-mado-scratch',
+  'aiya000/nvim-mado-scratch',
   -- dir = vim.fn.expand('~/Repository/nvim-mado-scratch'),
   -- dir = vim.fn.expand('~/Repository/nvim-mado-scratch/main'),
-  dir = vim.fn.expand('~/Repository/nvim-mado-scratch/copilot/show-file-name-in-float-window'),
+  dependencies = { 'nvim-lua/plenary.nvim' },
   opts = {
-    file_pattern = {
-      when_file_buffer = vim.fn.expand('~/tmp/scratch-%d'),
-    },
     default_file_ext = 'md',
-    default_open_method = {
-      method = 'float-aspect',
-      scale = { width = 0.8, height = 0.9 },
+    default_open_method = { method = 'sp', height = 15 },
+    auto_hide_buffer = {
+      when_tmp_buffer = true,  -- Auto-hide temporary buffers
+      when_file_buffer = true, -- Auto-hide persistent buffers
     },
   },
 }
