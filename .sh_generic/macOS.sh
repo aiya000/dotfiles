1abdeaa504d1da7581329694920809cbdfc74ef0
aiya000 <aiya000.develop@gmail.com>
2026-01-14 13:47:05 +0900

    feat(shell > aliases): Add `ps-sorted-by-mem` alias for macOS
    Shows processes sorted by memory usage with CPU%, user, and command columns.

--- .sh_generic/macOS.sh
+++ .sh_generic/macOS.sh
@@ -3,6 +3,7 @@ export BROWSER=open
 
 alias x=open
 alias ps='ps -e -o pid,lstart,command | sort -k2,6'  # Show pid, started time, and the command. Sort by start time
+alias ps-sorted-by-mem='\ps -e -o %mem,%cpu,user,command | drop 1 | cut -c1-120 | sort -k 1 -n && echo "-----------------------------------" && \ps -e -o %mem,%cpu,user,command | head -1'
 alias docker-force-remove-containers-data='rm -rf ~/Library/Containers/com.docker.docker/Data/*'
 
 i_have gsed && alias sed=gsed
