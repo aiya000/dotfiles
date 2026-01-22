590caa8a4a73ffcaf9c0e86f39e35f3f6ee93171
aiya000 <aiya000.develop@gmail.com>
2026-01-22 18:23:45 +0900

    change(Neovim > luasnippets): Add `--mobile --local` flags to `notify-cascade` snippets

--- .config/nvim/lua/luasnippets/markdown/cli.lua
+++ .config/nvim/lua/luasnippets/markdown/cli.lua
@@ -12,7 +12,7 @@ return list.concat(
   {
     s(
       'notify_cascade_schedule_today',
-      fmt("notify-cascade {time_begin} '{title}' '{time_begin_} - {time_end}' 3h 1h 30m 15m 5m 1m", {
+      fmt("notify-cascade --mobile --local {time_begin} '{title}' '{time_begin_} - {time_end}' 3h 1h 30m 15m 5m 1m", {
         time_begin = i(1, 'time_begin'),
         time_begin_ = rep(1),
         time_end = i(2, 'time_end'),
@@ -22,7 +22,7 @@ return list.concat(
 
     s(
       'notify_cascade_schedule_someday',
-      fmt("notify-cascade '{date} {time_begin}' '{title}' '{time_begin_} - {time_end}' 3h 1h 30m 15m 5m 1m", {
+      fmt("notify-cascade --mobile --local '{date} {time_begin}' '{title}' '{time_begin_} - {time_end}' 3h 1h 30m 15m 5m 1m", {
         date = i(1, 'date'),
         time_begin = i(2, 'time_begin'),
         time_begin_ = rep(2),
