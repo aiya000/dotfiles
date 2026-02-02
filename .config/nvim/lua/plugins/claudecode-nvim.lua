6d582e496a10b3abd42da8d80e2b588b842f0256
aiya000 <aiya000.develop@gmail.com>
2026-02-02 19:17:30 +0900

    fix(Neovim > plugins > claudecode.nvim): Fix `<leader>cc` to correctly working (previously, first `<leader>cc` doesn't open in git root)

--- .config/nvim/lua/plugins/claudecode-nvim.lua
+++ .config/nvim/lua/plugins/claudecode-nvim.lua
@@ -1,52 +1,5 @@
-local fn = require('utils.functions')
-
 ---A key to spawn claudecode.nvim
 local toggle_key = '<leader>cc'
-
----Checks if window is actually displayed (has height/width)
----@param win integer --An element of `vim.api.nvim_list_wins()`
-local function is_win_displayed(win)
-  local win_config = vim.api.nvim_win_get_config(win)
-  return win_config.hide
-    or (win_config.height ~= nil and win_config.height == 0)
-    or (win_config.width ~= nil and win_config.width == 0)
-end
-
----Does the buffer typically contain 'claude' in its name ClaudeCode terminal?
----@param win integer --An element of `vim.api.nvim_list_wins()`
-local function is_buffer_name_claude(win)
-  local buf = vim.api.nvim_win_get_buf(win)
-  local bufname = vim.api.nvim_buf_get_name(buf)
-  return bufname:match('claude') or vim.bo[buf].filetype == 'claudecode'
-end
-
----NOTE: なぜか`:ClaudeCodeFocus`でfloat windowがトグルしないので、ワークアラウンド
----Checks if ClaudeCode window/buffer is currently visible (not hidden)
-local function is_claudecode_window_opening()
-  return vim.iter(vim.api.nvim_list_wins()):any(function(win) ---@param win integer
-    return vim.api.nvim_win_is_valid(win) and not is_win_displayed(win) and is_buffer_name_claude(win)
-  end)
-end
-
-local function toggle()
-  if is_claudecode_window_opening() then
-    vim.cmd('ClaudeCodeFocus')
-    return
-  end
-
-  local cwd = vim.fn.getcwd()
-  fn.try_finally(function()
-    vim.cmd('lcd ' .. InitLua.path_at_started)
-    vim.cmd('ClaudeCode --continue')
-    return nil
-  end, function()
-    vim.cmd('lcd ' .. cwd)
-  end)
-end
-
-local function toggle_docker()
-  vim.cmd('ClaudeCodeDocker')
-end
 
 return {
   'coder/claudecode.nvim',
@@ -62,10 +15,10 @@ return {
     'ClaudeCodeDiffDeny',
   },
   keys = {
-    { toggle_key, mode = { 'n' }, toggle, desc = 'Toggle Claude Code' },
+    { toggle_key, mode = { 'n' }, '<Cmd>ClaudeCodeFocus<CR>', desc = 'Toggle Claude Code' },
     { '<leader>cr', mode = { 'n' }, '<Cmd>ClaudeCode --resume<CR>', desc = 'Resume Claude' },
     { '<leader>cC', mode = { 'n' }, '<Cmd>ClaudeCode<CR>', desc = 'New Claude' },
-    { '<leader>cD', mode = { 'n' }, toggle_docker, desc = 'Toggle Docker Claude' },
+    { '<leader>cD', mode = { 'n' }, '<Cmd>ClaudeCodeDocker<CR>', desc = 'Toggle Docker Claude' },
     { '<leader>cM', mode = { 'n' }, '<Cmd>ClaudeCodeSelectModel<CR>', desc = 'Select Claude model' },
     { '<leader>cb', mode = { 'n' }, '<Cmd>ClaudeCodeAdd %<CR>', desc = 'Add current buffer' },
     { '<leader>cs', mode = { 'n' }, 'V:ClaudeCodeSend<CR>', desc = 'Send to Claude' },
