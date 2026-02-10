77197e28fa77ef43227283b74251e31569fdf51c
aiya000 <aiya000.develop@gmail.com>
2026-02-10 16:20:17 +0900

    feat(Neovim > ftplugin > terminal-shell): Add `I` & `A` keymaps for line-edge navigation

--- .config/nvim/after/ftplugin/terminal-shell.lua
+++ .config/nvim/after/ftplugin/terminal-shell.lua
@@ -1,1 +1,11 @@
+local nvim = require('nvim')
+
 vim.keymap.set('n', 'p', 'pi', { buffer = true }) -- Enter insert mode after pasting
+
+vim.keymap.set('n', 'I', function()
+  nvim.feedkeys('i<C-a>')
+end, { buffer = true })
+
+vim.keymap.set('n', 'A', function()
+  nvim.feedkeys('i<C-e>')
+end, { buffer = true })
