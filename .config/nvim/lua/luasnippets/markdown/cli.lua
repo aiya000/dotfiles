378e3d0c281be6e2ed9ebb92624b73d6efe9418e
aiya000 <aiya000.develop@gmail.com>
2026-02-19 19:13:09 +0900

    change(Neovim > luasnippets > cli): Replace `--mobile` with `--ntfy` in `notify-cascade` snippets & Add `10:00` timing to someday variant

--- .config/nvim/lua/luasnippets/markdown/cli.lua
+++ .config/nvim/lua/luasnippets/markdown/cli.lua
@@ -11,7 +11,7 @@ local t = ls.text_node
 return list.concat({
   s(
     'notify_cascade_schedule_today',
-    fmt("notify-cascade --mobile --local {time_begin} '{title}' '{time_begin_} - {time_end}' now 3h 1h 30m 15m 5m 1m", {
+    fmt("notify-cascade --ntfy --local {time_begin} '{title}' '{time_begin_} - {time_end}' now 3h 1h 30m 15m 5m 1m", {
       time_begin = i(1, 'time_begin'),
       time_begin_ = rep(1),
       time_end = i(2, 'time_end'),
@@ -22,7 +22,7 @@ return list.concat({
   s(
     'notify_cascade_schedule_someday',
     fmt(
-      "notify-cascade --mobile --local '{date} {time_begin}' '{title}' '{time_begin_} - {time_end}' '{date_} 09:30' 3h 1h 30m 15m 5m 1m",
+      "notify-cascade --ntfy --local '{date} {time_begin}' '{title}' '{time_begin_} - {time_end}' '{date_} 09:30' 10:00 3h 1h 30m 15m 5m 1m",
       {
         date = i(1, 'date'),
         date_ = rep(1),
