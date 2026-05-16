914f1aa25369edd0d24a42bbe183916d487e2df0
aiya000 <aiya000.develop@gmail.com>
2026-05-17 00:32:45 +0900

    fix(Shell > neovim): Use \`\$NVIM_PARENT_ADDRESS\` instead of \`\$NEOVIM_TERMINAL\` for nvim alias
    $NEOVIM_TERMINAL is intentionally nil on Windows filesystem to avoid a
    terminal-close bug, so the nvim alias was never set there. $NVIM_PARENT_ADDRESS
    is always set by Neovim regardless of filesystem, making it the correct condition.

--- .shells/aliases/neovim.sh
+++ .shells/aliases/neovim.sh
@@ -2,8 +2,9 @@
 
 alias vterminal=$'nvim +"lua require(\'nvim\').termopen_shell()"'
 
-# See ~/.config/nvim/lua/helper.lua for $NEOVIM_TERMINAL
-if [[ $NEOVIM_TERMINAL != '' ]] ; then
+# Use NVIM_PARENT_ADDRESS (always set by Neovim) instead of NEOVIM_TERMINAL
+# (NEOVIM_TERMINAL is intentionally nil on Windows filesystem to avoid a terminal-close bug)
+if [[ $NVIM_PARENT_ADDRESS != '' ]] ; then
   export PAGER=cat
   alias nvim=nvim-parent-tabnew
   alias s=nvim-parent-split
