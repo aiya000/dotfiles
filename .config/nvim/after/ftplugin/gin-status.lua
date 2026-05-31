7d3f46d7dfe5f701559841872d5c8317987b34af
aiya000 <aiya000.develop@gmail.com>
2026-05-31 18:31:13 +0900

    feat(Neovim > gin-status): Open commit buffer in new tab from float & Extract \`run_closing_float()\` helper

--- .config/nvim/after/ftplugin/gin-status.lua
+++ .config/nvim/after/ftplugin/gin-status.lua
@@ -63,9 +63,31 @@ local function run_add_patch()
   vim.fn.feedkeys('i', 'n') -- Enter insert mode
 end
 
+local function close_win_later(win_id)
+  vim.schedule(function()
+    if vim.api.nvim_win_is_valid(win_id) then
+      vim.api.nvim_win_close(win_id, true)
+    end
+  end)
+end
+
+---@param action fun(): nil
+local function run_closing_float(action)
+  local win = nvim.is_in_float_window() and vim.api.nvim_get_current_win() or nil
+  action()
+  if win ~= nil then
+    close_win_later(win)
+  end
+end
+
 ---@param subcmd? string[] --`:Gin commit --verbose {subcmd (concatenated)}`
 local function open_commit_buffer(subcmd)
   local git_commit = vim.fn.extendnew({ 'Gin', 'commit', '--verbose' }, subcmd or {})
+  if nvim.is_in_float_window() then
+    local win = vim.api.nvim_get_current_win()
+    vim.cmd('tabnew')
+    close_win_later(win)
+  end
   vim.cmd(table.concat(git_commit, ' '))
 end
 
@@ -84,14 +106,6 @@ local function delete_this_file()
   end)
 end
 
-local function close_win_later(win_id)
-  vim.schedule(function()
-    if vim.api.nvim_win_is_valid(win_id) then
-      vim.api.nvim_win_close(win_id, true)
-    end
-  end)
-end
-
 vim.keymap.set('n', 'Q', '<Cmd>bdelete!<CR>', { buffer = true, silent = true })
 vim.keymap.set('n', 'A', run_add_patch, { buffer = true, silent = true })
 
@@ -104,24 +118,24 @@ vim.keymap.set('n', 'o', function()
     return
   end
 
-  local float_win = vim.api.nvim_get_current_win()
-  local filepath = get_current_line_file_path()
-  if filepath == nil then
-    vim.notify('Failed to parse the current line for file path', vim.log.levels.ERROR)
-    return
-  end
+  run_closing_float(function()
+    local float_win = vim.api.nvim_get_current_win()
+    local filepath = get_current_line_file_path()
+    if filepath == nil then
+      vim.notify('Failed to parse the current line for file path', vim.log.levels.ERROR)
+      return
+    end
 
-  if InitLua.git_root ~= nil then
-    filepath = InitLua.git_root .. '/' .. filepath
-  end
+    if InitLua.git_root ~= nil then
+      filepath = InitLua.git_root .. '/' .. filepath
+    end
 
-  local ok, prev_win = pcall(vim.api.nvim_win_get_var, float_win, 'gin_status_prev_win')
-  close_win_later(float_win)
-
-  if ok and vim.api.nvim_win_is_valid(prev_win) then
-    vim.api.nvim_set_current_win(prev_win)
-  end
-  vim.cmd('edit ' .. vim.fn.fnameescape(filepath))
+    local ok, prev_win = pcall(vim.api.nvim_win_get_var, float_win, 'gin_status_prev_win')
+    if ok and vim.api.nvim_win_is_valid(prev_win) then
+      vim.api.nvim_set_current_win(prev_win)
+    end
+    vim.cmd('edit ' .. vim.fn.fnameescape(filepath))
+  end)
 end, { buffer = true, silent = true })
 
 vim.keymap.set('n', '<C-r>', '<Cmd>GinStatus<CR>', { buffer = true, silent = true }) -- TODO: `gin#util#reload()`が使えそう
@@ -131,9 +145,9 @@ vim.keymap.set('n', 'p', function()
     nvim.run_with_virtual_keymaps('<Plug>(gin-action-diff:smart:vsplit)')
     return
   end
-  local win = vim.api.nvim_get_current_win()
-  nvim.run_with_virtual_keymaps('<Plug>(gin-action-diff:smart:tabedit)')
-  close_win_later(win)
+  run_closing_float(function()
+    nvim.run_with_virtual_keymaps('<Plug>(gin-action-diff:smart:tabedit)')
+  end)
 end, { buffer = true, silent = true, nowait = true })
 
 vim.keymap.set('n', 'P', ':<C-u>!git push', { remap = true, buffer = true })
@@ -153,13 +167,11 @@ vim.keymap.set('n', 'D', delete_this_file, { buffer = true })
 vim.keymap.set('n', '<C-g>', ':<C-u>!git<Space>', { nowait = true, remap = true, buffer = true, silent = true }) -- remap to open cmdpalette
 
 vim.keymap.set('n', 'O', function()
-  local win = nvim.is_in_float_window() and vim.api.nvim_get_current_win() or nil
   vim.cmd('normal "zyy')
-  vim.cmd('tabnew')
-  vim.cmd('edit ' .. vim.fn.trim(vim.fn.getreg('z')))
-  if win ~= nil then
-    close_win_later(win)
-  end
+  run_closing_float(function()
+    vim.cmd('tabnew')
+    vim.cmd('edit ' .. vim.fn.trim(vim.fn.getreg('z')))
+  end)
 end, { buffer = true, silent = true })
 
 vim.keymap.set('n', 'S', function()
