e3e654d77fee41ad94ef6e465097adfd6b057cf6
aiya000 <aiya000.develop@gmail.com>
2026-08-19 14:35:37 +0900

    fix(Neovim): Fix an issue where nvim-cmp autocompletion wasn't working when `init-lightweigh.lua` was loaded & Create nvim-lightweight command

--- .config/nvim/lua/luasnippets/all.lua
+++ .config/nvim/lua/luasnippets/all.lua
@@ -1,6 +1,11 @@
 local list = require('utils.list')
+local is_luasnip_emoji_ok, luasnip_emoji = pcall(require, 'luasnip-emoji')
 
 return {
-  snippets = list.concat(require('luasnippets.all.all'), require('luasnippets.all.kaomoji'), require('luasnip-emoji')),
+  snippets = list.concat(
+    require('luasnippets.all.all'),
+    require('luasnippets.all.kaomoji'),
+    is_luasnip_emoji_ok and luasnip_emoji or {}
+  ),
   autosnippets = {},
 }
