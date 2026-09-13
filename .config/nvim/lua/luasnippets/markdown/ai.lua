b4f266cc229e57779b115839dec93484d5b96588
aiya000 <aiya000.develop@gmail.com>
2026-09-13 17:00:28 +0900

    feat(Neovim > luasnippets): Add `instruct_rename_this_project_now` snippet expanding to `/rename`
    The project name comes from the git-root directory name, falling back to the
    current directory name outside a git repository.
    
    Also drops the `ultrathink`/`ult` and `git_commit` snippets, and switches the
    autocompact aliases to `sm()`.

--- .config/nvim/lua/luasnippets/markdown/ai.lua
+++ .config/nvim/lua/luasnippets/markdown/ai.lua
@@ -3,34 +3,36 @@ local list = require('utils.list')
 local ls = require('luasnip')
 local sm = require('utils.luasnip').sm
 
+local f = ls.function_node
 local i = ls.insert_node
 local s = ls.snippet
 local t = ls.text_node
 
-return list.concat(
-  -- TODO: なんかできない。多分smの実装？ 修正する
-  -- sm({ 'ultrathink', 'ult' }, t('ultrathink'))
-  {
-    s('ultrathink', t('ultrathink')),
-    s('ult', t('ultrathink')),
-  },
+---現在のプロジェクト名を推論する
+---@return string --gitリポジトリならgit-rootのディレクトリ名、そうでなければカレントディレクトリのディレクトリ名
+local function detect_project_name()
+  local git_root = require('git').read_git_root()
+  local project_dir = git_root ~= nil and git_root or vim.fn.getcwd()
+  return vim.fn.fnamemodify(project_dir, ':t')
+end
 
-  {
-    s(
-      'instruct_for_autocompact',
-      t('/compact 昔の履歴は圧縮して、最近の履歴は圧縮しないようにして')
-    ),
-    s('autocompact', t('/compact 昔の履歴は圧縮して、最近の履歴は圧縮しないようにして')),
-  },
+return list.concat(
+  sm(
+    { 'instruct_for_autocompact', 'autocompact' },
+    t('/compact 昔の履歴は圧縮して、最近の履歴は圧縮しないようにして')
+  ),
 
   {
     s(
       'instruct_for_git_commit',
       t('`~/.dotfiles/.claude_global/commands/git-commit.md`のルールを参照して、`git commit`を実行して')
     ),
+
     s(
-      'git_commit',
-      t('`~/.dotfiles/.claude_global/commands/git-commit.md`のルールを参照して、`git commit`を実行して')
+      'instruct_rename_this_project_now',
+      f(function()
+        return ('/rename %s %s'):format(detect_project_name(), os.date('%Y-%m-%d %H:%M'))
+      end)
     ),
   }
 )
