6a49b6b0de6c2f9723641a7478826d5510f94e2b
aiya000 <aiya000.develop@gmail.com>
2026-09-16 10:38:28 +0900

    change(Neovim): Rename `init_env(_post).lua` to `nvim_init_env(_post).lua`
    Update the sourced file names from `~/init_env.lua` and `~/init_env_post.lua` to `~/nvim_init_env.lua` and `~/nvim_init_env_post.lua`.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -208,7 +208,7 @@ if vim.fn.filereadable(init_private_lua) == 1 then
   vim.cmd.source(init_private_lua)
 end
 
-local init_env_lua = vim.fn.expand('~/init_env.lua')
+local init_env_lua = vim.fn.expand('~/nvim_init_env.lua')
 if vim.fn.filereadable(init_env_lua) == 1 then
   vim.cmd.source(init_env_lua)
 end
@@ -233,7 +233,7 @@ require('keymaps')
 require('commands') -- Due to `Require('commands')`, 'commands' module put here instead of `./plugins/commands.lua`
 require('color-presets')
 
-local init_env_post_lua = vim.fn.expand('~/init_env_post.lua')
+local init_env_post_lua = vim.fn.expand('~/nvim_init_env_post.lua')
 if vim.fn.filereadable(init_env_post_lua) == 1 then
   vim.cmd.source(init_env_post_lua)
 end
